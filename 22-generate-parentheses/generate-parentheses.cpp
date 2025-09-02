4cb2fe8cb3e5a636e0fff9b2865d0dcc97821b11
akonic <45717770+akonic@users.noreply.github.com>
2025-09-02 12:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (83.03%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void backtrack(int n, int open, int close, string& s, vector<string>& ans) {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+        
+        if (open < n) {
+            s.push_back('(');
+            backtrack(n, open + 1, close, s, ans);
+            s.pop_back();
+        }
+        if (close < open) {
+            s.push_back(')');
+            backtrack(n, open, close + 1, s, ans);
+            s.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s;
+        backtrack(n, 0, 0, s, ans);
+        return ans;
+    }
+};
