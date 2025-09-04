a93f990ceb15f7850595ff14b5aa9ef38401f225
akonic <45717770+akonic@users.noreply.github.com>
2025-09-05 00:15:15 +0530

    Time: 30 ms (21.64%) | Memory: 22.6 MB (18.80%) - LeetSync

---
+++ 690-employee-importance/employee-importance.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        
+        int ans=0;
+        int n=employees.size();
+        map<int,int> m;
+        map<int,vector<int>> adj;
+        for(auto& i : employees)
+        {
+            m[i->id]=i->importance;
+            for(auto& j : i->subordinates)
+            {
+                adj[i->id].push_back(j);
+            }
+        }
+         map<int,int> visited;
+         queue<int> q;
+        q.push(id);
+        
+        while(!q.empty())
+        {
+            int id=q.front();
+            ans+=m[id];
+            q.pop();
+            visited[id]=1;
+            for(auto& j : adj[id] )
+            {
+                if(visited[j]==0)
+                {
+                    q.push(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
