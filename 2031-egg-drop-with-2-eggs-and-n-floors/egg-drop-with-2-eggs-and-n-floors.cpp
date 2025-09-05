073f0f8c854efe13ca309ce6a76aa1f8bf45183a
akonic <45717770+akonic@users.noreply.github.com>
2025-09-06 00:11:22 +0530

    Time: 69 ms (16.59%) | Memory: 7.8 MB (80.79%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int dp[3][1001];
-    int fun(int e,int f)
-    {
-        if(f==0 || f==1)
-        {
+
+    int fun(int e, int f) {
+        if (f == 0 || f == 1)
             return f;
-        }
-        if(e==1)
-        {
+        if (e == 1)
             return f;
-        }
-        if(dp[e][f]!=-1)
-        {
+        if (dp[e][f] != -1)
             return dp[e][f];
+
+        int ans = INT_MAX;
+        for (int k = 1; k <= f; ++k) {
+            int broken = fun(e - 1, k - 1);
+            int notBroken = fun(e, f - k);
+            int worst = 1 + max(broken, notBroken);
+            ans = min(ans, worst);
         }
-        int ans=INT_MAX;
-        for(int k=1;k<=f;k++)
-        {
-            int temp=1+max(fun(e-1,k-1),fun(e,f-k));
-            ans=min(ans,temp);
-        }
-        return dp[e][f]=ans;
+        return dp[e][f] = ans;
     }
-       
+
     int twoEggDrop(int n) {
-         memset(dp,-1,sizeof(dp));
-        return fun(2,n);
+        memset(dp, -1, sizeof(dp));
+        return fun(2, n);
     }
 };
