91744bba25401f7dd736320b5cc522d5ce541b11
akonic <45717770+akonic@users.noreply.github.com>
2025-09-07 16:49:48 +0530

    Time: 4 ms (80.96%) | Memory: 10.9 MB (87.57%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,33 +1,56 @@
 class Solution {
 public:
-    int dp[501][501];
-    long fun(string word1, string word2,int i,int j)
-    {
-        if(i>=word1.size() && j>=word2.size())
-        {
-            return 0;
-        }
-        if(i>=word1.size() ||  j>=word2.size())
-        {
-            return max(word2.size() - j,word1.size() - i);
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        long ans=0;
-        if(word1[i]==word2[j])
-        {
-            ans=fun(word1,word2,i+1,j+1);
-        }
-       else
-        {
-            ans=min({1+fun(word1,word2,i+1,j),1+fun(word1,word2,i+1,j+1),1+fun(word1,word2,i,j+1)});
-        }
-        return dp[i][j]=ans;
-    }
+    // int dp[501][501];
+    // long fun(string word1, string word2,int i,int j)
+    // {
+    //     if(i>=word1.size() && j>=word2.size())
+    //     {
+    //         return 0;
+    //     }
+    //     if(i>=word1.size() ||  j>=word2.size())
+    //     {
+    //         return max(word2.size() - j,word1.size() - i);
+    //     }
+    //     if(dp[i][j]!=-1)
+    //     {
+    //         return dp[i][j];
+    //     }
+    //     long ans=0;
+    //     if(word1[i]==word2[j])
+    //     {
+    //         ans=fun(word1,word2,i+1,j+1);
+    //     }
+    //    else
+    //     {
+    //         ans=min({1+fun(word1,word2,i+1,j),1+fun(word1,word2,i+1,j+1),1+fun(word1,word2,i,j+1)});
+    //     }
+    //     return dp[i][j]=ans;
+    // }
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        return fun(word1,word2,0,0);
+        //memset(dp,-1,sizeof(dp));
+        int n=word1.size(),m=word2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=j;
+                }
+                else if(j==0)
+                {
+                    dp[i][j]=i;
+                }
+                else if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
