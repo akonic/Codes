8349f96e8dd8eb5bf6fd13556aa483901790891a
akonic <45717770+akonic@users.noreply.github.com>
2025-09-07 18:40:40 +0530

    Time: 26 ms (94.39%) | Memory: 67.7 MB (90.56%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
+    int fun(const string &s, unordered_set<string>& st, int i, vector<int>& v, int maxLen) {
+        if (i >= (int)s.size()) return 0;
+        if (v[i] != -1) return v[i];
 
-    int fun(string s,set<string>& st,int i, vector<int>& v)
-    {
-        if(i>=s.size())
-        {
-            return 0;
-        }
-        if(v[i]!=-1)
-        {
-            return v[i];
-        }
-        int ans=INT_MAX;
+        int ans = INT_MAX;
         string temp;
-        for(int k=i;k<s.size();k++)
-        {
-            int c=0;
-            temp+=s[k];
-            if(st.find(temp)==st.end())
-            {
-                c+=temp.size();;
-            }
-            c+=fun(s,st,k+1,v);
-            ans=min(ans,c);
+        for (int k = i; k < (int)s.size() && k - i + 1 <= maxLen; ++k) {
+            temp += s[k];
+            int extra = (st.find(temp) == st.end()) ? (k - i + 1) : 0;
+            int sub = fun(s, st, k + 1, v, maxLen);
+            if (sub != INT_MAX) ans = min(ans, extra + sub);
         }
-        return v[i]=ans;
-
+        return v[i] = ans;
     }
+
     int minExtraChar(string s, vector<string>& dictionary) {
-         set<string> st;
-        for(int i=0;i<dictionary.size();i++)
-        {
-            st.insert(dictionary[i]);
-        }
-        vector<int> v(s.size()+1,-1);
-        return fun(s,st,0,v);
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
+        int maxLen = 0;
+        for (auto &w : dictionary) maxLen = max(maxLen, (int)w.size());
+
+        int n = s.size();
+        vector<int> v(n + 1, -1);
+        return fun(s, st, 0, v, maxLen);
     }
 };
