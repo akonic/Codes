1adc7a2f7ce226dde7244f7365553b3d34d1d0cf
akonic <45717770+akonic@users.noreply.github.com>
2025-09-20 22:50:48 +0530

    Time: 51 ms (88.99%) | Memory: 18 MB (90.69%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,40 +1,39 @@
+
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        vector<vector<pair<int,int>>> adj(n);
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                adj[i].push_back({j,abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])});
-                adj[j].push_back({i,abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])});
-            }
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-        int sum=0;
-        vector<int> cost(n,0);
-        cost[0]=0;
-        q.push({0,0});
-        while(!q.empty())
-        {
-            int cst=q.top().first;
-            int node=q.top().second;
-            q.pop();
-            if(cost[node]) continue;
+        int n = points.size();
+        if (n == 0) return 0;
 
-            cost[node]=1;
-            sum+=cst;
+        auto dist = [&](int i, int j) {
+            return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+        };
 
-            for(auto& i : adj[node])
-            {
-                if(cost[i.first]==0)
-                {
-                    q.push({i.second,i.first});
+        vector<int> cost(n, INT_MAX);             
+        vector<char> inMST(n, 0);                
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        cost[0] = 0;
+        pq.push({0, 0});
+        long long ans = 0;
+
+        while (!pq.empty()) {
+            auto [w, u] = pq.top(); pq.pop();
+            if (inMST[u]) continue;     
+            inMST[u] = 1;
+            ans += w;                 
+
+            for (int v = 0; v < n; ++v) {
+                if (!inMST[v]) {
+                    int d = dist(u, v);
+                    if (d < cost[v]) {
+                        cost[v] = d;
+                        pq.push({d, v});   
+                    }
                 }
             }
         }
-        return sum;
+
+        return (int)ans;
     }
-  
 };
