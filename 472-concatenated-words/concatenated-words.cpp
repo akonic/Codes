c1a9248fea862afc5156f8b236dcc15a51ba6330
akonic <45717770+akonic@users.noreply.github.com>
2025-09-23 21:39:18 +0530

    Time: 463 ms (8.95%) | Memory: 69.7 MB (35.37%) - LeetSync

---
+++ 472-concatenated-words/concatenated-words.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    set<string> s;
+    bool fun(string word,int i,int j,vector<int>& dp)
+    {
+       if (i == j) return true; 
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        for (int k = i + 1; k <= j; k++) {  
+            if (s.find(word.substr(i, k - i)) != s.end()) {
+                if (fun(word, k, j,dp)) {
+                    dp[i]=1;
+                    return true;
+                    }
+            }
+        }
+        dp[i]=0;
+        return false;
+    }
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        s.clear();
+        for(auto& i:words)
+        {
+            s.insert(i);
+        }
+        vector<string> v;
+        for(auto& i: words)
+        {
+            s.erase(i);
+            int n=i.size();
+            vector<int> dp(n+1,-1);
+            if (fun(i,0,n,dp)) v.push_back(i);
+            s.insert(i);
+        }
+
+        return v;
+
+    }
+};
