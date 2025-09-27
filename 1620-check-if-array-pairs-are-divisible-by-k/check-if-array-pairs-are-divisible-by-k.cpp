82d2c52c79358978d178f045888f3424d99ffab1
akonic <45717770+akonic@users.noreply.github.com>
2025-09-27 20:54:22 +0530

    Time: 51 ms (28.91%) | Memory: 89.8 MB (8.81%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         int c=0;
-        map<int,int> m;
+        unordered_map<int,int> m;
         int n=arr.size();
         vector<int> v;
         for(int i=0;i<n;i++)
         {
             int x = ((arr[i] % k) + k) % k;
-            if(x==0)
-            {
-                c++;
-            }
-            else {
-           v.push_back(x);}
+            
+            m[x]++;
         }
-        if(c%2!=0)
+        if(m[0]%2!=0)
         {
             return false;
         }
-        sort(v.begin(),v.end());
-        int i=0,j=v.size()-1;
-        while(i<j)
-        {
-            if((v[i]+v[j])%k!=0)
-            {
-                return false;
-            }
-            i++;j--;
+       for (int i = 1; i <= k/2; ++i) {
+        if (i != k - i) {                   
+            if (m[i] != m[k - i]) return false;
+        } else {                          
+            if (m[i] % 2 != 0) return false;
         }
+    }
         return true;
     }
 };
