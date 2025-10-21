10c58d1bf4be7f7f9edf5a98d5c6ac129a6ac4ed
akonic <45717770+akonic@users.noreply.github.com>
2025-10-21 21:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (97.60%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj,vector<bool>& visited,int i,int& c)
-    {
+    void dfs(vector<vector<int>>& rooms, vector<bool>& visited, int i, int& c) {
         c++;
-        visited[i]=1;
-        for(int j=0;j<adj[i].size();j++)
-        {
-            if(!visited[adj[i][j]])
-            {
-                dfs(adj,visited,adj[i][j],c);
+        visited[i] = 1;
+        for (int j = 0; j < rooms[i].size(); j++) {
+            if (!visited[rooms[i][j]]) {
+                dfs(rooms, visited, rooms[i][j], c);
             }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<rooms[i].size();j++)
-            {
-                adj[i].push_back(rooms[i][j]);
-            }
+        int n = rooms.size();
+        vector<bool> visited(n, 0);
+        int c = 0;
+        dfs(rooms,visited,0,c);
+        if (c == n) {
+            return true;
         }
-        vector<bool> visited(n,0);
-        int c=0;
-            dfs(adj,visited,0,c);
-            if(c==n)
-            {
-                return true;
-            }
         return false;
     }
 };
