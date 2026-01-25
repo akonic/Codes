dad499d81af3c51a3b73039d1695de027f2d9ec6
akonic <45717770+akonic@users.noreply.github.com>
2026-01-26 01:29:41 +0530

    Time: 20 ms (8.48%) | Memory: 52.1 MB (80.22%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -4,15 +4,14 @@ class Solution {
         int x=n/3;
         List<Integer> list = new ArrayList<>();
         HashMap<Integer,Integer> mp = new HashMap<>();
+        HashMap<Integer,Integer> pp = new HashMap<>();
         for(int i=0;i<n;i++)
         {
             mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
-        }
-        for(Map.Entry<Integer,Integer> e : mp.entrySet())
-        {
-            if(e.getValue()>x)
+            if(mp.get(nums[i])>x && !pp.containsKey(nums[i]))
             {
-                list.add(e.getKey());
+                list.add(nums[i]);
+                pp.put(nums[i],1);
             }
         }
         return list;
