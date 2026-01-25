e97349ec085f45eaa2c09a6ca22d61186db3b99f
akonic <45717770+akonic@users.noreply.github.com>
2026-01-25 20:34:03 +0530

    Time: 8 ms (6.26%) | Memory: 48.1 MB (5.98%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int romanToInt(String s) {
+        TreeMap<Character,Integer> mp = new TreeMap<>();
+        mp.put('I',1);
+        mp.put('V',5);
+        mp.put('X',10);
+        mp.put('L',50);
+        mp.put('C',100);
+        mp.put('D',500);
+        mp.put('M',1000);
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='I' && i+1<n && (s.charAt(i+1)=='V' || s.charAt(i+1)=='X'))
+            {
+               ans-=mp.get(s.charAt(i));
+               i++;
+               ans+=mp.get(s.charAt(i));
+            }
+            else if(s.charAt(i)=='X' && i+1<n && (s.charAt(i+1)=='L' || s.charAt(i+1)=='C'))
+            {
+               ans-=mp.get(s.charAt(i));
+               i++;
+               ans+=mp.get(s.charAt(i));
+            }
+            else if(s.charAt(i)=='C' && i+1<n && (s.charAt(i+1)=='D' || s.charAt(i+1)=='M'))
+            {
+               ans-=mp.get(s.charAt(i));
+               i++;
+               ans+=mp.get(s.charAt(i));
+            }
+            else{
+                ans+=mp.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+}
