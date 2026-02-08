15e78d3244ccf240fc5a1f1a5de8bd6459db64e7
akonic <45717770+akonic@users.noreply.github.com>
2026-02-08 20:45:12 +0530

    Time: 25 ms (87.63%) | Memory: 81.3 MB (10.35%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,40 +1,33 @@
 class Solution {
-    public int checkPartition(int[] nums,int i,int W,int[][] dp)
-    {
-        if(W==0)
-        {
-            return 1; 
-        }
-        if(W<0 || i>=nums.length)
-        {
+    public int checkPartition(int[] nums, int i, int W, int[][] dp) {
+        if (W == 0)
+            return 1;
+        if (W < 0 || i >= nums.length)
             return 0;
-        }
-        if(dp[W][i]!=-1)
-        {
+
+        if (dp[W][i] != -1)
             return dp[W][i];
+        if (checkPartition(nums, i + 1, W - nums[i], dp) == 1) {
+            return dp[W][i] = 1;
         }
-        int a=checkPartition(nums,i+1,W-nums[i],dp);
-        int b=checkPartition(nums,i+1,W,dp);
-        
-        return  dp[W][i]= Math.max(a,b);
-        
+
+        return dp[W][i] = checkPartition(nums, i + 1, W, dp);
+
     }
+
     public boolean canPartition(int[] nums) {
-        int n=nums.length;
-        int W=0;
-        for(int i : nums)
-        {
-            W+=i;
+        int n = nums.length;
+        int W = 0;
+        for (int i : nums) {
+            W += i;
         }
-        if(W%2!=0)
-        {
+        if (W % 2 != 0) {
             return false;
         }
-        int[][] dp = new int[W/2 +1][n+1];
-        for(int[] rows : dp)
-        {
-            Arrays.fill(rows,-1);
+        int[][] dp = new int[W / 2 + 1][n + 1];
+        for (int[] rows : dp) {
+            Arrays.fill(rows, -1);
         }
-        return checkPartition(nums, 0, W/2, dp) == 1;
+        return checkPartition(nums, 0, W / 2, dp) == 1;
     }
 }
