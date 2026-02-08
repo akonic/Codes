018f6713eea842209c92fe378831778c75ebd458
akonic <45717770+akonic@users.noreply.github.com>
2026-02-08 21:17:36 +0530

    Time: 8 ms (60.39%) | Memory: 47.3 MB (10.99%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -4,11 +4,10 @@ class Solution {
         if (i == nums.length) {
             return x == target ? 1 : 0;
         }
-        if(dp[sum+x][i]!=-1)
-        {
-            return dp[sum+x][i];
+        if (dp[i][sum + x] != -1) {
+            return dp[i][sum + x];
         }
-        return dp[sum+x][i]=fun(nums,i+1,target,x-nums[i],sum,dp) + fun(nums,i+1,target,x+nums[i],sum,dp);
+        return dp[i][sum + x]=fun(nums,i+1,target,x-nums[i],sum,dp) + fun(nums,i+1,target,x+nums[i],sum,dp);
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n =nums.length;
@@ -17,7 +16,7 @@ class Solution {
         {
             sum+=i;
         }
-        int[][] dp = new int[2*sum+1][n];
+       int[][] dp = new int[n][2 * sum + 1];
         for(int[] rows : dp)
         {
             Arrays.fill(rows,-1);
