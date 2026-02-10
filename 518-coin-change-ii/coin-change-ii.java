c9f91d4da5df61a677ffb8effb052dd0bc8d1662
akonic <45717770+akonic@users.noreply.github.com>
2026-02-10 18:27:02 +0530

    Time: 27 ms (18.75%) | Memory: 56.8 MB (41.46%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int fun(int[] coins,int amount,int index,int[][] dp)
+    {
+        if(amount==0)
+        {
+            return 1;
+        }
+        if(amount<0 || index>=coins.length)
+        {
+            return 0;
+        }
+        if(dp[index][amount]!=-1)
+        {
+            return dp[index][amount];
+        }
+        int a=0,b=0;
+        if(amount-coins[index]>=0)
+        {
+            a=fun(coins,amount-coins[index],index,dp);
+        }
+        b=fun(coins,amount,index+1,dp);
+
+        return dp[index][amount]=a+b;
+        
+    }
+    public int change(int amount, int[] coins) {
+        int n=coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        for(int[] rows : dp)
+        {
+            Arrays.fill(rows,-1);
+        }
+        return fun(coins,amount,0,dp);
+    }
+}
