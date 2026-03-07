075df8796c636acfb0b84c5bce94e7c1958f9208
akonic <45717770+akonic@users.noreply.github.com>
2026-03-08 01:55:36 +0530

    Time: 2 ms (57.12%) | Memory: 44.2 MB (46.49%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int sum = nums[0], n = nums.length;
+        int p = Integer.MIN_VALUE, ans = Integer.MAX_VALUE;
+         for (int i = 0; i < n; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+         }
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                p = Math.max(p, sum);
+                sum = nums[i];
+                int a=p;
+                while (mp.getOrDefault(a, 0) != 0) {
+                    a++;
+                }
+                return a;
+            }
+
+        }
+        System.out.println(sum);
+        int a=sum;
+        while (mp.getOrDefault(a, 0) != 0) {
+                    a++;
+                }
+
+        return a;
+    }
+}
