77612f493aa03ec9aa445f3cff28ba39b7b123fa
akonic <45717770+akonic@users.noreply.github.com>
2026-03-29 16:45:32 +0530

    Time: 13 ms (5.00%) | Memory: 48.8 MB (23.39%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,58 @@
+class Solution {
+    // private void helper(ArrayList<ArrayList<Integer>> adj,int[] visited,int i)
+    // {
+    //     visited[i]=1;
+    //     for(int j : adj.get(i))
+    //     {
+    //         if(visited[j]==0)
+    //         {
+    //             helper(adj,visited,j);
+    //         }
+    //     }
+    // }
+    public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int n = isConnected.length;
+        int[] visited = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            visited[i]=0;
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1){
+                adj.get(i).add(j);
+                }
+            }
+        }
+        int count =0;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i]==0)
+            {
+                count++;
+                q.offer(i);
+                while(!q.isEmpty())
+                {
+                    int p=q.peek();
+                    q.poll();
+                    visited[p]=1;
+                    for(int j : adj.get(p))
+                    {
+                        if(visited[j]==0)
+                        {
+                            q.offer(j);
+                        }
+                    }
+                }
+            }
+        }
+        
+       
+        return count;
+    }
+}
