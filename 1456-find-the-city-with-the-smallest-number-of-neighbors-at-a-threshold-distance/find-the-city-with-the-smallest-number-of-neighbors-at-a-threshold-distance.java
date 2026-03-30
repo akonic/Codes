3e28da742fc0ad26d14ebd0dcaf45e350fc6132f
akonic <45717770+akonic@users.noreply.github.com>
2026-03-31 03:57:17 +0530

    Time: 51 ms (11.81%) | Memory: 47.2 MB (15.15%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,62 +1,51 @@
 class Solution {
-    private void helper(ArrayList<ArrayList<int[]>> adj,int[] dist,int i)
-    {
-         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-         dist[i] = 0;
-        pq.offer(new int[]{0, i});
-
+    private void helper(ArrayList<ArrayList<int[]>> adj, int[] dist, int i) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        dist[i] = 0;
+        pq.offer(new int[] { 0,i });
         while (!pq.isEmpty()) {
             int[] top = pq.poll();
-            int d = top[0];
-            int u = top[1];
+            int node = top[1];
+            int dis = top[0];
+            for (int[] dest : adj.get(node)) {
+                int newNode = dest[0];
+                int newDist = dest[1];
 
-            if (d > dist[u])
-                continue;
-
-            for (int p[] : adj.get(u)) {
-                int v = p[0];
-                int w = p[1];
-
-                if (dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
-                    pq.offer(new int[]{dist[v], v});
+                if (dis + newDist < dist[newNode]) {
+                    dist[newNode] = dis + newDist;
+                    pq.offer(new int[] { dist[newNode], newNode }); 
                 }
             }
         }
 
     }
+
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<edges.length;i++)
-        {
-            int a=edges[i][0];
-            int b=edges[i][1];
-            int c=edges[i][2];
-            adj.get(a).add(new int[]{b,c});
-            adj.get(b).add(new int[]{a,c});
+        for (int i = 0; i < edges.length; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int c = edges[i][2];
+            adj.get(a).add(new int[] { b, c });
+            adj.get(b).add(new int[] { a, c });
         }
-        int node=Integer.MIN_VALUE,c=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++)
-        {
-            int[] dist=new int[n];
-            int p=0;
-            Arrays.fill(dist,Integer.MAX_VALUE);
-            helper(adj,dist,i);
-            for(int j=0;j<n;j++)
-            {
-                if(i!=j && dist[j]<=distanceThreshold)
-                {
+        int node = Integer.MIN_VALUE, c = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int[] dist = new int[n];
+            int p = 0;
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            helper(adj, dist, i);
+            for (int j = 0; j < n; j++) {
+                if (i != j && dist[j] <= distanceThreshold) {
                     p++;
                 }
             }
-            if(c>=p)
-            {
-                c=Math.min(c,p);
-                node=Math.max(node,i);
+            if (c >= p) {
+                c = Math.min(c, p);
+                node = Math.max(node, i);
             }
         }
         return node;
