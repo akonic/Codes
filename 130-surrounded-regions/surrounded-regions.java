88c3bc7aeea932a7001c960826988459986e7d64
akonic <45717770+akonic@users.noreply.github.com>
2026-04-05 23:08:10 +0530

    Time: 2 ms (79.27%) | Memory: 47.9 MB (29.91%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,40 +1,35 @@
 class Solution {
 
-    void dfs(char[][] board,int[][] visited,int i,int j,int n,int m)
+    void dfs(char[][] board,int i,int j,int n,int m)
     {
-        visited[i][j]=1;
-        if(i+1<n && board[i+1][j]=='O' && visited[i+1][j]!=1)
+        board[i][j]='P';
+        if(i+1<n && board[i+1][j]=='O')
         {
-            dfs(board,visited,i+1,j,n,m);
+            dfs(board,i+1,j,n,m);
         }
-        if(j+1<m && board[i][j+1]=='O' && visited[i][j+1]!=1)
+        if(j+1<m && board[i][j+1]=='O')
         {
-            dfs(board,visited,i,j+1,n,m);
+            dfs(board,i,j+1,n,m);
         }
-        if(i-1>=0 && board[i-1][j]=='O' && visited[i-1][j]!=1)
+        if(i-1>=0 && board[i-1][j]=='O')
         {
-            dfs(board,visited,i-1,j,n,m);
+            dfs(board,i-1,j,n,m);
         }
-        if(j-1>=0 && board[i][j-1]=='O' && visited[i][j-1]!=1)
+        if(j-1>=0 && board[i][j-1]=='O')
         {
-            dfs(board,visited,i,j-1,n,m);
+            dfs(board,i,j-1,n,m);
         }
     }
     public void solve(char[][] board) {
         int n=board.length;
         int m=board[0].length;
-        int[][] visited = new int[n][m];
-        for(int i=0;i<n;i++)
-        {
-            Arrays.fill(visited[i],0);
-        }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j]=='O'&& visited[i][j]==0 && (i==0 || j==0 || i==n-1 || j==m-1))
+                if(board[i][j]=='O' && (i==0 || j==0 || i==n-1 || j==m-1))
                 {
-                    dfs(board,visited,i,j,n,m);
+                    dfs(board,i,j,n,m);
                 }
             }
         }
@@ -42,7 +37,11 @@ class Solution {
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j]=='O' && visited[i][j]!=1)
+                if(board[i][j]=='P')
+                {
+                    board[i][j]='O';
+                }
+                else if(board[i][j]=='O')
                 {
                     board[i][j]='X';
                 }
