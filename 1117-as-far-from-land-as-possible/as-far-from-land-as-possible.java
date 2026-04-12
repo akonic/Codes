a57a300d41b9b97086b0f17796b207982a34d666
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-13 00:11:58 +0530

    Time: 15 ms (58.03%) | Memory: 47.5 MB (61.39%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -1,46 +1,35 @@
 class Solution {
     public int maxDistance(int[][] grid) {
-        int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
-        int n=grid.length;
-        int m=grid[0].length;
+        int n = grid.length, m = grid[0].length;
+        int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
         int[][] ans = new int[n][m];
-        for(int i=0;i<n;i++)
-        {
-            Arrays.fill(ans[i],-1);
-        }
-        Queue<Integer> q = new ArrayDeque<>();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    q.offer(i*m + j);
-                    ans[i][j]=0;
-                }
-            }
-        }
-        if(q.isEmpty() || q.size() == n*m) return -1;
-        int result = -1;
-        while(!q.isEmpty())
-        {
-            int top = q.poll();
-            int x=top/m;
-            int y=top%m;
+        for (int i = 0; i < n; i++)
+            Arrays.fill(ans[i], -1);
 
-            for(int[] dir : dirs)
-            {
-                int nx= x+dir[0];
-                int ny=y+dir[1];
-                if(nx>=0 && ny>=0 && nx<n && ny<m && ans[nx][ny]==-1)
-                {
-                    q.offer(nx*m + ny);
-                    ans[nx][ny]=ans[x][y]+1;
-                    result=Math.max(result,ans[nx][ny]);
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == 1) {
+                    ans[i][j] = 0;
+                    q.offer(i * m + j);
+                }
+
+        if (q.isEmpty() || q.size() == n * m)
+            return -1;
+
+        int result = -1;
+        while (!q.isEmpty()) {
+            int top = q.poll();
+            int x = top / m, y = top % m;
+            for (int[] dir : dirs) {
+                int nx = x + dir[0], ny = y + dir[1];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && ans[nx][ny] == -1) {
+                    ans[nx][ny] = ans[x][y] + 1;
+                    result = Math.max(result, ans[nx][ny]);
+                    q.offer(nx * m + ny);
                 }
             }
         }
         return result;
-
     }
 }
