0129c948722037d9fe0415e9c2f2c81187d709f9
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-13 00:10:08 +0530

    Time: 14 ms (86.78%) | Memory: 47.4 MB (76.39%) - LeetSync

---
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int maxDistance(int[][] grid) {
+        int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] ans = new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(ans[i],-1);
+        }
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    q.offer(i*m + j);
+                    ans[i][j]=0;
+                }
+            }
+        }
+        int result = -1;
+        while(!q.isEmpty())
+        {
+            int top = q.poll();
+            int x=top/m;
+            int y=top%m;
+
+            for(int[] dir : dirs)
+            {
+                int nx= x+dir[0];
+                int ny=y+dir[1];
+                if(nx>=0 && ny>=0 && nx<n && ny<m && ans[nx][ny]==-1)
+                {
+                    q.offer(nx*m + ny);
+                    ans[nx][ny]=ans[x][y]+Math.abs(x-nx)+Math.abs(y-ny);
+                    result=Math.max(result,ans[nx][ny]);
+                }
+            }
+        }
+        return result;
+
+    }
+}
