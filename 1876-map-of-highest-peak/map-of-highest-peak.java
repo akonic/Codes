f3462967cf7fcbe2c3452fc64f763387b1a9395c
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-12 20:15:50 +0530

    Time: 41 ms (89.97%) | Memory: 177.4 MB (86.41%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.java
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -3,11 +3,9 @@ class Solution {
         int n=isWater.length;
         int m=isWater[0].length;
         int[][] ans = new int[n][m];
-        int[][] visited = new int[n][m];
         for(int i=0;i<n;i++)
         {
-            Arrays.fill(visited[i],0);
-            Arrays.fill(ans[i],0);
+            Arrays.fill(ans[i],-1);
         }
         Queue<Integer> q = new ArrayDeque<>();
         for(int i=0;i<n;i++)
@@ -17,7 +15,7 @@ class Solution {
                 if(isWater[i][j]==1)
                 {
                     q.offer(i*m + j);
-                    visited[i][j]=1;
+                    ans[i][j]=0;
                 }
             }
         }
@@ -34,9 +32,8 @@ class Solution {
                 {
                     int nx=x+dir[0];
                     int ny=y+dir[1];
-                    if(nx>=0 && nx<n && ny>=0 && ny<m && visited[nx][ny]==0 && isWater[nx][ny]==0)
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && ans[nx][ny]==-1 && isWater[nx][ny]==0)
                     {
-                        visited[nx][ny]=1;
                         ans[nx][ny]=ans[x][y]+1;
                         q.offer(nx*m + ny);
                     }
