4e72a292cca0f97b789fb3ea1db50a34a9ae62d9
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-15 00:57:12 +0530

    Time: 4 ms (51.24%) | Memory: 46.7 MB (90.65%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        TreeMap<Integer,Integer> mp = new TreeMap<>();
-        int n=nums.length;
-        int[] arr = new int[2];
-        for(int i =0;i<n;i++)
-        {
-           mp.put(nums[i],i);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], i);
         }
-        for(int i=0;i<n;i++)
-        {
-            if(mp.containsKey(target-nums[i]) && i!=mp.get(target-nums[i]))
-            {
-               arr[0]=i;
-               arr[1]=mp.get(target-nums[i]);
-               return arr;
+        for (int i = 0; i < n; i++) {
+            int ans = target - nums[i];
+            if (map.containsKey(ans)) {
+                int ind = map.get(ans);
+                if (ind != i) {
+                    return new int[] { i, ind };
+                }
             }
         }
-        return arr;
+
+        return new int[] {};
     }
 }
