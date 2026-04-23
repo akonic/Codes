5ed3b36bb1d4402f2f883eefe9860eb976338927
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-24 03:15:15 +0530

    Time: 29 ms (8.05%) | Memory: 65.8 MB (16.99%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,34 +1,20 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        Map<Integer,int[]> mp = new HashMap<>();
-        int n=nums.length;
-        int sum=0;
-        mp.put(0, new int[]{0,Integer.MIN_VALUE});
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==0)
-            {
-                sum-=1;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int n = nums.length;
+        int sum = 0;
+        int ans = 0;
+        mp.put(0, -1);
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i] == 0 ? -1 : 1;
+
+            if (mp.containsKey(sum)) {
+                ans = Math.max(ans, i - mp.get(sum));
+            } else {
+                mp.put(sum, i);
             }
-            else{
-                sum+=1;
-            }
-            if(!mp.containsKey(sum))
-            {
-                mp.put(sum, new int[]{i+1,Integer.MIN_VALUE});
-            }
-            else{
-                int a=mp.get(sum)[0];
-                mp.put(sum, new int[]{a,i+1});
-            }
-             
-        }
-        int ans=0;
-        for (int[] key : mp.values()) {
-            if(key[0]!=Integer.MIN_VALUE && key[1]!=Integer.MIN_VALUE){
-            ans=Math.max(ans,key[1]-key[0]);}
         }
         return ans;
-
     }
 }
