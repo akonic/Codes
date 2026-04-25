4ef61e2e751e5f9e1e2b58b1caa730cc9e1cd2b7
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-25 22:37:47 +0530

    Time: 6 ms (25.66%) | Memory: 43.5 MB (60.16%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] != 0) {
+                if (citations[i] <= n - i ) {
+                    ans = Math.max(ans, citations[i]);
+                }
+                else{
+                    ans=Math.max(ans,n-i);
+                }
+            }
+        }
+        return ans;
+    }
+}
