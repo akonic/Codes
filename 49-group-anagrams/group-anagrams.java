79a9603fc58405570056852b67fabb11db7da6f0
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-27 02:35:05 +0530

    Time: 2658 ms (5.02%) | Memory: 49.2 MB (95.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,39 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-
-        TreeMap<String, List<String>> mp = new TreeMap<>();
-        int n = strs.length;
-        for (int i = 0; i < n; i++) {
-            char[] arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            String sorted = new String(arr);
-            mp.computeIfAbsent(sorted, k -> new ArrayList<>())
-                    .add(strs[i]);
+    private boolean areSame(String temp, String temp2) {
+        int[] map = new int[26];
+        for (int i = 0; i < temp.length(); i++) {
+            map[temp.charAt(i) - 'a']++;
         }
+        for (int i = 0; i < temp2.length(); i++) {
+            map[temp2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (map[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ls = new ArrayList<>();
-        for (Map.Entry<String, List<String>> e : mp.entrySet()) {
-            ls.add(e.getValue());
+        int n = strs.length;
+        int[] visited = new int[n];
+        for (int i = 0; i < n; i++) {
+            String temp = strs[i];
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                List<String> ls2 = new ArrayList<>();
+                ls2.add(temp);
+                for (int j = i + 1; j < n; j++) {
+                    String temp2 = strs[j];
+                    if (visited[j] == 0 && areSame(temp, temp2)) {
+                        visited[j] = 1;
+                        ls2.add(temp2);
+                    }
+                }
+                ls.add(ls2);
+            }
         }
         return ls;
     }
