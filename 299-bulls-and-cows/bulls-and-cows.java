bfb19ee246e6bc593c9da22d7db959e6af9827af
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-04-27 01:56:12 +0530

    Time: 4 ms (49.03%) | Memory: 43.9 MB (25.23%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -2,11 +2,11 @@ class Solution {
     public String getHint(String secret, String guess) {
         int n=secret.length();
         int a=0,b=0;
-        HashMap<Character,Integer> mp = new HashMap<>();
+        int[] mp = new int[26];
         for(int i=0;i<n;i++)
         {
             Character ch = secret.charAt(i);
-            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            mp[ch-'0']++;
         }
         for(int i=0;i<n;i++)
         {
@@ -15,7 +15,7 @@ class Solution {
             if(ch==ch2)
             {
                 a++;
-                 mp.put(ch,mp.getOrDefault(ch,0)-1);
+                 mp[ch-'0']--;
             }
         }
         for(int i=0;i<n;i++)
@@ -23,10 +23,10 @@ class Solution {
             Character ch = secret.charAt(i);
             Character ch2 = guess.charAt(i);
            
-                if(ch!=ch2 && mp.containsKey(ch2) && mp.get(ch2)>0)
+                if(ch!=ch2 && mp[ch2-'0']!=0)
                 {
                     b++;
-                    mp.put(ch2,mp.getOrDefault(ch2,0)-1);
+                     mp[ch2-'0']--;
                 }
         }
         String p = a+"A"+b+"B";
