b87d46d6e8f29320f686c538f98d61ea9f9c4aa2
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-02 00:34:02 +0530

    Time: 4 ms (90.35%) | Memory: 90.9 MB (11.53%) - LeetSync

--- 396-rotate-function/rotate-function.java
+++ 396-rotate-function/rotate-function.java
@@ -2,13 +2,12 @@ class Solution {
     public int maxRotateFunction(int[] nums) {
         int n=nums.length;
         int[] pre = new int[n];
-        int sum=0;
+        int totalSum=0;
         for(int i=0;i<n;i++)
         {
-            sum+=nums[i];
-            pre[i]=sum;
+            totalSum+=nums[i];
         }
-        sum=0;
+        int sum=0;
         int ans=Integer.MIN_VALUE;
         for(int i=0;i<n;i++)
         {
@@ -17,16 +16,8 @@ class Solution {
         ans=Math.max(ans,sum);
         for(int i=n-1;i>=0;i--)
         {
-            int pref=0,suf=0;
-            if(i>0)
-            {
-                pref=pre[i-1];
-            }
-            if(i<n-1)
-            {
-                suf=pre[n-1]-pre[i];
-            }
-            sum+=pref+suf-(n-1)*nums[i];
+           
+            sum+=totalSum-nums[i]-(n-1)*nums[i];
             ans=Math.max(ans,sum);
         }
         return ans;
