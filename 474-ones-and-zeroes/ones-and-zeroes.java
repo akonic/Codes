241c56b136e2f3354ffa0bc3c7e4679c21d47a69
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-08 01:39:19 +0530

    Time: 70 ms (18.60%) | Memory: 86.4 MB (20.75%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,59 +1,48 @@
-class Pair {
-    int first;
-    int second;
-
-    Pair(int f, int s) {
-        first = f;
-        second = s;
-    }
-}
-
 class Solution {
-    public int fun(int m, int n, int i, List<Pair> list,int[][][] dp) {
-        if (m < 0 || n < 0)
-            return Integer.MIN_VALUE;
-        if (i >= list.size())
-            return 0;
-        if(dp[i][m][n]!=-1)
+    int as;
+    private int helper(int[][] ls, int i, int n, int m,int[][][] dp) {
+        if(i>=ls.length)
         {
-            return dp[i][m][n];
+            return 0;
         }
-        int a = 0, b = 0;
-        int x = list.get(i).first, y = list.get(i).second;
-        if (m - x >= 0 && n - y >= 0) {
-            a = 1 + fun(m - x, n - y, i + 1, list,dp);
+        if(dp[i][n][m]!=-1)
+        {
+            return dp[i][n][m];
         }
-        b = fun(m, n, i + 1, list,dp);
-        return dp[i][m][n]=Math.max(a, b);
+        int a=0,b=0;
+        if (n - ls[i][0] >= 0 && m - ls[i][1] >= 0) {
+           a= 1+helper(ls, i + 1, n - ls[i][0], m - ls[i][1],dp);
+        }
+        b=helper(ls, i + 1, n, m,dp);
+        return dp[i][n][m]=Math.max(a,b);
     }
 
     public int findMaxForm(String[] strs, int m, int n) {
-        List<Pair> list = new ArrayList<>();
-        int x = strs.length;
-        for (int i = 0; i < x; i++) {
+        int len = strs.length;
+        int[][] ls = new int[len][2];
+        for (int i = 0; i < len; i++) {
+            char[] pp = strs[i].toCharArray();
+            int newLen = pp.length;
             int a = 0, b = 0;
-            String st = strs[i];
-            int p = st.length();
-            for (int j = 0; j < p; j++) {
-                if (st.charAt(j) == '0') {
+            for (int j = 0; j < newLen; j++) {
+                if (pp[j] == '1') {
                     a++;
                 } else {
                     b++;
                 }
             }
-            list.add(new Pair(a, b));
+            ls[i][0] = a;
+            ls[i][1] = b;
         }
-        int ans = 0;
-        int[][][] dp = new int [list.size()+1][m+1][n+1];
-        for(int[][] rows : dp)
+        int[][][] dp = new int[len+1][n+1][m+1];
+        for(int i=0;i<=len;i++)
         {
-            for(int[] cols : rows)
+            for(int j=0;j<=n;j++)
             {
-                Arrays.fill(cols,-1);
+                Arrays.fill(dp[i][j],-1);
             }
         }
-        int p = fun(m, n, 0, list,dp);
-        return p;
-
+       return helper(ls, 0, n, m,dp);
+       // return as;
     }
 }
