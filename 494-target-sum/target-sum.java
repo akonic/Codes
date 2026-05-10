b5b3322abfbc8e85be6c388a1c4f3db019d31f4f
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-10 22:42:59 +0530

    Time: 8 ms (60.81%) | Memory: 46.8 MB (21.10%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,33 +1,26 @@
 class Solution {
-    private int helper(int[] nums, int target,int sum, int i,int[][] dp,int totalSum) {
-        if (i == nums.length && sum == target) {
-            return 1;
-        }
-        if (i == nums.length) {
+    private int helper(int[] nums, int target, int sum, int i, int[][] dp, int totalSum) {
+        if (i == nums.length)
+            return sum == target ? 1 : 0;
+        if (Math.abs(sum) > totalSum)
             return 0;
-        }
-        if(dp[i][totalSum+sum]!=-1)
-        {
-            return dp[i][totalSum+sum];
-        }
-        int a=0,b=0;
-        a += helper(nums, target, sum + nums[i], i + 1,dp,totalSum);
-        b += helper(nums, target, sum - nums[i], i + 1,dp,totalSum);
-        return dp[i][totalSum+sum]=a+ b;
+        if (dp[i][totalSum + sum] != -1)
+            return dp[i][totalSum + sum];
+
+        return dp[i][totalSum + sum] = helper(nums, target, sum + nums[i], i + 1, dp, totalSum) +
+                helper(nums, target, sum - nums[i], i + 1, dp, totalSum);
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum=0;
+        int totalSum = 0;
         int n = nums.length;
-        for(int i : nums)
-        {
-            totalSum+=i;
+        for (int i : nums) {
+            totalSum += i;
         }
-        int[][] dp = new int[n+1][2*totalSum+1];
-        for(int[] i : dp)
-        {
-            Arrays.fill(i,-1);
+        int[][] dp = new int[n + 1][2 * totalSum + 1];
+        for (int[] i : dp) {
+            Arrays.fill(i, -1);
         }
-        return helper(nums, target, 0, 0,dp,totalSum);
+        return helper(nums, target, 0, 0, dp, totalSum);
     }
 }
