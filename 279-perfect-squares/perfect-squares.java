61b9b684014eb7ff4f7516ab99f94fc2293b457a
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-14 23:00:55 +0530

    Time: 522 ms (5.01%) | Memory: 64.4 MB (5.35%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,31 +1,40 @@
 class Solution {
-
-    static final int INF = (int)1e9;
-
-    public int fun(int n, int i, int[][] dp) {
-        if (n == 0) return 0;
-
-        if (i * i > n) return INF; 
-
-        if (dp[n][i] != -1) {
+    private int helper(int n,int i,List<Integer> ls,int[][] dp)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        if(i==ls.size() || n<0)
+        {
+            return Integer.MAX_VALUE;
+        }
+        if(dp[n][i]!=-1)
+        {
             return dp[n][i];
         }
-
-        int take = INF, skip;
-        take = 1 + fun(n - i * i, i, dp);
-        skip = fun(n, i + 1, dp);
-
-        return dp[n][i] = Math.min(take, skip);
-    }
-
-    public int numSquares(int n) {
-        int maxI = (int)Math.sqrt(n);
-        int[][] dp = new int[n + 1][maxI + 1];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        int a=helper(n-ls.get(i),i,ls,dp);
+        if(a!=Integer.MAX_VALUE)
+        {
+            a++;
         }
-
-        return fun(n, 1, dp);
+        int b=helper(n,i+1,ls,dp);
+        return dp[n][i] = Math.min(a,b);
+        
+    }
+    public int numSquares(int n) {
+        int x=1;
+        List<Integer> ls = new ArrayList<>();
+        while(x*x<=n)
+        {
+            ls.add(x*x);
+            x++;
+        }
+        int[][] dp = new int[n+1][ls.size()+1];
+        for(int[] i : dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        return helper(n,0,ls,dp);
     }
 }
