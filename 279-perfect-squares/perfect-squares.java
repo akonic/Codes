27f2cee6684acde2b9a56394cf670dd3c62d037b
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-15 01:19:08 +0530

    Time: 33 ms (63.15%) | Memory: 44.6 MB (51.50%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,40 +1,40 @@
 class Solution {
-    private int helper(int n,int i,List<Integer> ls,int[][] dp)
-    {
-        if(n==0)
-        {
-            return 0;
-        }
-        if(i==ls.size() || n<0)
-        {
-            return Integer.MAX_VALUE;
-        }
-        if(dp[n][i]!=-1)
-        {
-            return dp[n][i];
-        }
-        int a=helper(n-ls.get(i),i,ls,dp);
-        if(a!=Integer.MAX_VALUE)
-        {
-            a++;
-        }
-        int b=helper(n,i+1,ls,dp);
-        return dp[n][i] = Math.min(a,b);
+    // private int helper(int n,int i,List<Integer> ls,int[][] dp)
+    // {
+    //     if(n==0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(i==ls.size() || n<0)
+    //     {
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(dp[n][i]!=-1)
+    //     {
+    //         return dp[n][i];
+    //     }
+    //     int a=helper(n-ls.get(i),i,ls,dp);
+    //     if(a!=Integer.MAX_VALUE)
+    //     {
+    //         a++;
+    //     }
+    //     int b=helper(n,i+1,ls,dp);
+    //     return dp[n][i] = Math.min(a,b);
         
-    }
+    // }
     public int numSquares(int n) {
-        int x=1;
-        List<Integer> ls = new ArrayList<>();
-        while(x*x<=n)
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
         {
-            ls.add(x*x);
-            x++;
+            int j=1;
+            while(j*j<=i)
+            {
+                dp[i]=Math.min(dp[i],dp[i-j*j]+1);
+                j++;
+            }
         }
-        int[][] dp = new int[n+1][ls.size()+1];
-        for(int[] i : dp)
-        {
-            Arrays.fill(i,-1);
-        }
-        return helper(n,0,ls,dp);
+        return dp[n];
     }
 }
