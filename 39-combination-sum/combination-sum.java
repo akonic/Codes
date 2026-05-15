84daec0d24fc422e65ab6bc44e77edcb6bbb421b
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-16 01:46:16 +0530

    Time: 3 ms (33.49%) | Memory: 45.8 MB (32.56%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,31 +1,26 @@
 class Solution {
-    Set<List<Integer>> ls = new HashSet<>();
-    private void helper(int[] candidates, int target,int i,List<Integer> temp)
-    {
-        if(target==0)
-        {
-           ls.add(new ArrayList<>(temp));
-            return ;  
+    List<List<Integer>> ls = new ArrayList<>();
+
+    private void helper(int[] candidates, int target, int i, List<Integer> temp) {
+        if (target == 0) {
+            ls.add(new ArrayList<>(temp));
+            return;
         }
-        int a=0;
-        for(int j=i;j<candidates.length;j++)
-        {
-            int newTarget = target-candidates[j];
-            if(newTarget>=0)
-            {
-                temp.add(candidates[j]);
-                helper(candidates,newTarget,j,temp);
-                temp.remove(temp.size() - 1);
-            }
+
+        for (int j = i; j < candidates.length; j++) {
+            int newTarget = target - candidates[j];
+            if (newTarget < 0)
+                break;
+            temp.add(candidates[j]);
+            helper(candidates, newTarget, j, temp);
+            temp.remove(temp.size() - 1);
         }
-        return ;
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        ls.clear();
+        ls = new ArrayList<>();
         Arrays.sort(candidates);
-        List<Integer> temp = new ArrayList<>();
-        helper(candidates,target,0,temp);
-        List<List<Integer>> result = new ArrayList<>(ls);
-        return result;
+        helper(candidates, target, 0, new ArrayList<>());
+        return ls;
     }
 }
