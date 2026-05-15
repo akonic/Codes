4b691692b9a725b5b21210bdfcf016ea41c1658d
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-16 01:03:34 +0530

    Time: 5 ms (5.35%) | Memory: 43.2 MB (8.16%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int helper(int[] nums,int i,int target,int[][] dp)
+    {
+        if(target==0)
+        {
+            return 1;
+        }
+        if(dp[i][target]!=-1)
+        {
+            return dp[i][target];
+        }
+        int a = 0;
+        for(int j=0;j<nums.length;j++)
+        {
+            int newTarget = target-nums[j];
+            if(newTarget>=0)
+            {
+                a+=helper(nums,j,newTarget,dp);
+            }
+        }
+        return dp[i][target]=a;
+    }
+    public int combinationSum4(int[] nums, int target) {
+        int n=nums.length;
+        int[][] dp = new int[n+1][target+1];
+        for(int[] i : dp){
+        Arrays.fill(i,-1);}
+      return helper(nums,0,target,dp);
+    }
+}
