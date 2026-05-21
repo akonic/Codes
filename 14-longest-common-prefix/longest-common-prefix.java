58ae9cb0ada9882934c0e6ac30d6c87d57501e61
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-22 01:21:48 +0530

    Time: 5 ms (5.67%) | Memory: 43.6 MB (8.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,18 +1,60 @@
+public class TrieNode{
+    TrieNode[] children;
+    boolean isEndOfWord;
+    public TrieNode(){
+        isEndOfWord=false;
+        children = new TrieNode[26];
+    }
+}
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        int n = strs.length;
-        int p=strs[0].length();
-        for(int i=1;i<n;i++)
-        {   int j=0;
-            int a=strs[i].length();
-            int b=strs[i-1].length();
-            while(j<a && j<b && strs[i].charAt(j)==strs[i-1].charAt(j))
+        static void insert(TrieNode root, String key)
+    {
+        TrieNode curr= root;
+        for(char c : key.toCharArray())
+        {
+            if(curr.children[c-'a']==null)
             {
-                j++;
+                TrieNode node = new TrieNode();
+                curr.children[c-'a']=node;
             }
-            p=Math.min(p,j);
+            curr = curr.children[c-'a'];
         }
-        return strs[0].substring(0,p);
+        curr.isEndOfWord=true;
+    }
+    static int search(TrieNode root,String key)
+    {
+        TrieNode curr = root;
+        int ans=0;
+        for(char c : key.toCharArray())
+        {
+            if(curr.children[c-'a']==null)
+            {
 
+                return ans;
+            }
+            ans++;
+            curr=curr.children[c-'a'];
+        }
+        //curr.isEndofWord;
+        return ans;
+    }
+    public String longestCommonPrefix(String[] strs) {
+        TrieNode trie = new TrieNode();
+        insert(trie,strs[0]);
+        int ans= Integer.MAX_VALUE;
+        for(int i=1;i<strs.length;i++)
+        {
+            ans=Math.min(ans,search(trie,strs[i]));
+        }
+        if(ans==Integer.MAX_VALUE)
+        {
+            ans=strs[0].length();
+        }
+        String s="";
+        for(int i=0;i<ans;i++)
+        {
+            s+=strs[0].charAt(i);
+        }
+        return s;
     }
 }
