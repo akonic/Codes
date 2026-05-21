19db4d748a7857e6beb5aa54337c0db69446310e
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-22 01:23:07 +0530

    Time: 1 ms (62.85%) | Memory: 43.5 MB (8.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,60 +1,49 @@
-public class TrieNode{
+public class TrieNode {
     TrieNode[] children;
     boolean isEndOfWord;
-    public TrieNode(){
-        isEndOfWord=false;
+    public TrieNode() {
+        isEndOfWord = false;
         children = new TrieNode[26];
     }
 }
-class Solution {
-        static void insert(TrieNode root, String key)
-    {
-        TrieNode curr= root;
-        for(char c : key.toCharArray())
-        {
-            if(curr.children[c-'a']==null)
-            {
-                TrieNode node = new TrieNode();
-                curr.children[c-'a']=node;
-            }
-            curr = curr.children[c-'a'];
-        }
-        curr.isEndOfWord=true;
-    }
-    static int search(TrieNode root,String key)
-    {
-        TrieNode curr = root;
-        int ans=0;
-        for(char c : key.toCharArray())
-        {
-            if(curr.children[c-'a']==null)
-            {
 
+class Solution {
+    static void insert(TrieNode root, String key) {
+        TrieNode curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEndOfWord = true;
+    }
+
+    static int search(TrieNode root, String key) {
+        TrieNode curr = root;
+        int ans = 0;
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (curr.children[c - 'a'] == null) {
                 return ans;
             }
             ans++;
-            curr=curr.children[c-'a'];
+            curr = curr.children[c - 'a'];
         }
-        //curr.isEndofWord;
         return ans;
     }
+
     public String longestCommonPrefix(String[] strs) {
         TrieNode trie = new TrieNode();
-        insert(trie,strs[0]);
-        int ans= Integer.MAX_VALUE;
-        for(int i=1;i<strs.length;i++)
-        {
-            ans=Math.min(ans,search(trie,strs[i]));
+        insert(trie, strs[0]);
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < strs.length; i++) {
+            ans = Math.min(ans, search(trie, strs[i]));
         }
-        if(ans==Integer.MAX_VALUE)
-        {
-            ans=strs[0].length();
+        if (ans == Integer.MAX_VALUE) {
+            ans = strs[0].length();
         }
-        String s="";
-        for(int i=0;i<ans;i++)
-        {
-            s+=strs[0].charAt(i);
-        }
-        return s;
+        return strs[0].substring(0, ans); 
     }
 }
