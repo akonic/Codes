45ab788c5faebd93d3266dc08643f5453fd665a9
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-29 21:56:00 +0530

    Time: 147 ms (24.36%) | Memory: 48.1 MB (98.06%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -0,0 +1,41 @@
+class Solution {
+    private boolean hasCommon(String a,String b)
+    {
+        int[] arr = new int[26];
+        for(int i=0;i<a.length();i++)
+        {
+            char ch = a.charAt(i);
+            arr[ch-'a']++;
+        }
+        for(int i=0;i<b.length();i++)
+        {
+            char ch = b.charAt(i);
+            if(arr[ch-'a']!=0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int maxProduct(String[] words) {
+        int ans=0;
+        int n=words.length;
+        Arrays.sort(words);
+        for(int i=n-1;i>=0;i--)
+        {
+            int len1=words[i].length();
+            for(int j=i-1;j>=0;j--)
+            {
+                int len2=words[j].length();
+                if(len1*len2>ans)
+                {
+                    if(!hasCommon(words[i],words[j]))
+                    {
+                        ans=len1*len2;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
