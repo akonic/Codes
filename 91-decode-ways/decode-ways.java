6520be66434d9472003510f2ec5660a543e10557
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-05-31 02:12:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (63.54%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -15,8 +15,7 @@ class Solution {
             b=helper(ch,i+1,n,dp);
             if(i+1<n)
             {
-                String p=""+ch[i]+ch[i+1];
-                int x = Integer.parseInt(p);
+                int x = (ch[i] - '0') * 10 + (ch[i+1] - '0');
                 if(x<27)
                 {
                     a=helper(ch,i+2,n,dp);
