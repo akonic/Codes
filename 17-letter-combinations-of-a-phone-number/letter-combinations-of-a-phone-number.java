4184a939fc099ed5389a5ba28528bc2d0a62cab2
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-06-26 22:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (78.08%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,36 +1,24 @@
 class Solution {
     List<String> ans = new ArrayList<>();
-    void dfs(String digits,int i,Map<Integer, List<Character>> mp,String temp)
-    {
-        if(i==digits.length())
-        {
-            ans.add(temp);
-            return ;
+    String[] keys = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    void dfs(String digits, int i, StringBuilder temp) {
+        if (i == digits.length()) {
+            ans.add(temp.toString());
+            return;
         }
 
-        int x = digits.charAt(i) - '0';
-        for(Character p : mp.get(x))
-        {
-            temp+=p;
-            dfs(digits,i+1,mp,temp);
-            temp = temp.substring(0, temp.length() - 1);
+        for (char p : keys[digits.charAt(i) - '0'].toCharArray()) {
+            temp.append(p);
+            dfs(digits, i + 1, temp);
+            temp.deleteCharAt(temp.length() - 1);
         }
     }
+
     public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return ans;
         ans = new ArrayList<>();
-        Map<Integer, List<Character>> mp = new HashMap<>();
-        mp.put(2, new ArrayList<>(Arrays.asList('a', 'b', 'c')));
-        mp.put(3, new ArrayList<>(Arrays.asList('d', 'e', 'f')));
-        mp.put(4, new ArrayList<>(Arrays.asList('g', 'h', 'i')));
-        mp.put(5, new ArrayList<>(Arrays.asList('j', 'k', 'l')));
-        mp.put(6, new ArrayList<>(Arrays.asList('m', 'n', 'o')));
-        mp.put(7, new ArrayList<>(Arrays.asList('p', 'q', 'r', 's')));
-        mp.put(8, new ArrayList<>(Arrays.asList('t', 'u', 'v')));
-        mp.put(9, new ArrayList<>(Arrays.asList('w', 'x', 'y', 'z')));
-
-        dfs(digits,0,mp,"");
-
+        dfs(digits, 0, new StringBuilder());
         return ans;
-
     }
 }
