eae0af42927447b57ba0216f9c600a9ec8f27132
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-04 21:20:03 +0530

    Time: 465 ms (5.04%) | Memory: 221.9 MB (5.04%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minScore(int n, int[][] roads) {
+        List<List<int[]>> ls = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            ls.add(new ArrayList<>());
+        }
+        for (int[] i : roads) {
+            int x = i[0];
+            int y = i[1];
+            int cost = i[2];
+            ls.get(x).add(new int[] { y, cost });
+            ls.get(y).add(new int[] { x, cost });
+        }
+        int[] costs = new int[n + 1];
+        Arrays.fill(costs,Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[] { 1, Integer.MAX_VALUE });
+        while (!pq.isEmpty()) {
+            int[] top = pq.poll();
+            int node = top[0];
+            int cost = top[1];
+            for (int[] i : ls.get(node)) {
+                int newNode = i[0];
+                int newCost = i[1];
+                if (costs[newNode] > Math.min(newCost,cost)) {
+                    pq.offer(new int[] { newNode, Math.min(newCost,cost) });
+                    costs[newNode] = Math.min(newCost,cost);
+                }
+            }
+        }
+        return costs[n];
+
+    }
+}
