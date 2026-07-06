4c13993f01f888370c44be05485e1bfaa5eefe4f
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-07 00:30:37 +0530

    Time: 8 ms (91.55%) | Memory: 49.1 MB (59.77%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,36 +1,30 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        int n=intervals.length;
-        List<int[]> ls = new ArrayList<>();
-        int x=intervals[0][0];
-        int y=intervals[0][1];
-        for(int i=1;i<n;i++)
-        {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int n = intervals.length;
+        int[][] ans = new int[n][2];
+        int idx = 0;
+        int x = intervals[0][0];
+        int y = intervals[0][1];
+        for (int i = 1; i < n; i++) {
             int a = intervals[i][0];
             int b = intervals[i][1];
-            if(y>=a && y>=b)
-            {
+            if (y >= a && y >= b) {
 
-            }
-            else if(y>=a && y<b)
-            {
-                y=b;
-            }
-            else if(y<a)
-            {
-                ls.add(new int[]{x,y});
-                x=a;y=b;
+            } else if (y >= a && y < b) {
+                y = b;
+            } else if (y < a) {
+                ans[idx][0] = x;
+                ans[idx][1] = y;
+                idx++;
+                x = a;
+                y = b;
             }
         }
-        ls.add(new int[]{x,y});
-        int p = ls.size();
-        int[][] ans = new int[p][2];
-        for(int i=0;i<p;i++)
-        {
-            ans[i][0]=ls.get(i)[0];
-            ans[i][1]=ls.get(i)[1];
-        }
-        return ans;
+        ans[idx][0] = x;
+        ans[idx][1] = y;
+        idx++;
+
+        return Arrays.copyOf(ans, idx);
     }
 }
