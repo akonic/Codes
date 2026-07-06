49d205151a9f7cf0287f0cde9e49523539746af3
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-07 00:25:06 +0530

    Time: 8 ms (91.55%) | Memory: 48.8 MB (91.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,19 +1,36 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int []> answer = new ArrayList<>();
-        int x = intervals[0][0], y = intervals[0][1];
-        for (int[] in : intervals) {
-            if (in[0] > y) {
-                answer.add(new int[] { x, y });
-                x = in[0];
-                y = in[1];
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        int n=intervals.length;
+        List<int[]> ls = new ArrayList<>();
+        int x=intervals[0][0];
+        int y=intervals[0][1];
+        for(int i=1;i<n;i++)
+        {
+            int a = intervals[i][0];
+            int b = intervals[i][1];
+            if(y>=a && y>=b)
+            {
+
             }
-            else{
-                y = Math.max(y, in[1]);
+            else if(y>=a && y<b)
+            {
+                y=b;
+            }
+            else if(y<a)
+            {
+                ls.add(new int[]{x,y});
+                x=a;y=b;
             }
         }
-        answer.add(new int[] { x, y });
-        return answer.toArray(new int[answer.size()][]);
+        ls.add(new int[]{x,y});
+        int p = ls.size();
+        int[][] ans = new int[p][2];
+        for(int i=0;i<p;i++)
+        {
+            ans[i][0]=ls.get(i)[0];
+            ans[i][1]=ls.get(i)[1];
+        }
+        return ans;
     }
 }
