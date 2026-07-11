4be1461fa9b4fe129976abe49be971c214a1ad1f
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-11 20:34:01 +0530

    Time: 11 ms (59.60%) | Memory: 47.4 MB (94.64%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,63 +1,41 @@
 class Solution {
-    private void dfs( List<List<Integer>> ls,int i, boolean[] visited,boolean[] check)
-    {
-        visited[i]=true;
-        check[i]=true;
-        for(int j : ls.get(i))
-        {
-            if(!visited[j])
-            {
-                dfs(ls,j,visited,check);
+    private void dfs(List<List<Integer>> ls, int i, boolean[] visited, List<Integer> component) {
+        visited[i] = true;
+        component.add(i);
+        for (int j : ls.get(i)) {
+            if (!visited[j]) {
+                dfs(ls, j, visited, component);
             }
         }
     }
+
     public int countCompleteComponents(int n, int[][] edges) {
         boolean[] visited = new boolean[n];
-        Arrays.fill(visited,false);
         List<List<Integer>> ls = new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
-            ls.add(new ArrayList<>());
+        for (int i = 0; i < n; i++) ls.add(new ArrayList<>());
+
+        for (int[] edge : edges) {
+            ls.get(edge[0]).add(edge[1]);
+            ls.get(edge[1]).add(edge[0]);
         }
-        for(int[] edge : edges)
-        {
-            int a= edge[0];
-            int b=edge[1];
-            ls.get(a).add(b);
-            ls.get(b).add(a);
-        }
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            if(!visited[i])
-            {
-                boolean[] check = new boolean[n];
-                Arrays.fill(check,false);
-                dfs(ls,i,visited,check);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                List<Integer> component = new ArrayList<>();
+                dfs(ls, i, visited, component);
+
+                int c = component.size() - 1;
                 boolean isComplete = true;
-                int c=0;
-                for(int j=0;j<n;j++)
-                {
-                    if(check[j])
-                    {
-                        c++;
+                for (int node : component) {
+                    if (ls.get(node).size() != c) {
+                        isComplete = false;
+                        break;
                     }
                 }
-                c--;
-                for(int j=0;j<n;j++)
-                {
-                    if(check[j] && ls.get(j).size()!=c)
-                    {
-                        isComplete=false;
-                    }
-                }
-                if(isComplete)
-                {
-                    ans++;
-                }
+                if (isComplete) ans++;
             }
         }
         return ans;
-
     }
 }
