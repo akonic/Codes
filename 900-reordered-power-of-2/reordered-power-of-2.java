2d5b89ab6925eef909a6501435834de49ebe5e81
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-13 22:58:48 +0530

    Time: 1 ms (98.71%) | Memory: 42.3 MB (84.05%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,53 +1,28 @@
 class Solution {
+    private static final int[][] SIGNATURES = new int[31][];
+    static {
+        long x = 1;
+        int i = 0;
+        while (x <= 1_000_000_000L) {
+            SIGNATURES[i++] = digitCount((int) x);
+            x *= 2;
+        }
+    }
+
     public boolean reorderedPowerOf2(int n) {
-        List<Integer> ls = new ArrayList<>();
-        int x=1;
-        while(x<=1000000000)
-        {
-            ls.add(x);
-            x*=2;
+        int[] check = digitCount(n);
+        for (int[] sig : SIGNATURES) {
+            if (Arrays.equals(sig, check)) return true;
         }
-        Map<Integer,int[]> mp = new HashMap<>();
-        for(int i : ls)
-        {
-            int temp=i;
-            int[] arr = new int[10];
-            while(temp>0)
-            {
-                arr[temp%10]++;
-                temp/=10;
-            }
-            // System.out.println(i);
-            // for(int c : arr)
-            // {
-            // System.out.println(c);}
-            // System.out.println("-------------");
-            mp.put(i,arr);
-        }
-        int[] check = new int[10];
-        int temp=n;
-        while(temp>0)
-        {
-            check[temp%10]++;
-            temp/=10;
-        }
-        for (Map.Entry<Integer, int[]> entry : mp.entrySet()) {
-            boolean flag=true;
-            int[] arr = entry.getValue();
-            for(int i=0;i<10;i++)
-            {
-                if(arr[i]!=check[i])
-                {
-                    flag=false;
-                }
-            }
-            if(flag)
-            {
-                return true;
-            }
-
-        }
-
         return false;
+    }
+
+    private static int[] digitCount(int num) {
+        int[] arr = new int[10];
+        while (num > 0) {
+            arr[num % 10]++;
+            num /= 10;
+        }
+        return arr;
     }
 }
