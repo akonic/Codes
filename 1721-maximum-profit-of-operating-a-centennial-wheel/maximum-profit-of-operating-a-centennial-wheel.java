f29316d0373cb804fb13ea946320dc7741f4a69a
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-15 00:19:08 +0530

    Time: 15 ms (5.88%) | Memory: 63.1 MB (52.94%) - LeetSync

--- 1721-maximum-profit-of-operating-a-centennial-wheel/maximum-profit-of-operating-a-centennial-wheel.java
+++ 1721-maximum-profit-of-operating-a-centennial-wheel/maximum-profit-of-operating-a-centennial-wheel.java
@@ -2,35 +2,25 @@ class Solution {
     public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
         int rotations = 0;
         int totalPeople = 0;
-        int currPeople=0;
-        int ans=0;
-        int i=0;
+        int currPeople = 0;
+        int ans = 0;
+        int i = 0;
         int n = customers.length;
-        int rot=-1;
-        boolean ran=true;
-        while(ran)
-        {
-            ran=false;
-            if(rotations>=i && i<n)
-            {
-                currPeople+=customers[i];
-                i++;
+        int rot = -1;
+        while (i < n || currPeople > 0) {
+            if (i < n) {
+                currPeople += customers[i++];
             }
-            int x= Math.min(4,currPeople);
-            totalPeople+=x;
-            currPeople-=x;
+            int x = (int) Math.min(4, currPeople);
+            totalPeople += x;
+            currPeople -= x;
             rotations++;
-            int currProfit=totalPeople*boardingCost - rotations*runningCost;
-            if(currProfit>ans)
-            {
-                ans=currProfit;
-                rot=rotations;
-            }
-            if(currPeople>0 || i<n)
-            {
-                ran=true;
-            }
 
+            long currProfit = totalPeople * boardingCost - (long) rotations * runningCost;
+            if (currProfit > ans) {
+                ans = (int)currProfit;
+                rot = rotations;
+            }
         }
         return rot;
     }
