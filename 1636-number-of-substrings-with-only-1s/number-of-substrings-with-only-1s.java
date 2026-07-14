775604b12723880748b685a26105d4a85c904783
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-14 23:02:40 +0530

    Time: 4 ms (94.82%) | Memory: 46.3 MB (70.83%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -1,32 +1,21 @@
 class Solution {
     public int numSub(String s) {
-        List<Integer> ls = new ArrayList<>();
-        char[] ch = s.toCharArray();
-        int n = ch.length;
+        final int mod = 1_000_000_007;
+        final long inv2 = (mod + 1) / 2;
+        long ans = 0;
         int c = 0;
+        int n = s.length();
         for (int i = 0; i < n; i++) {
-            if (ch[i] == '1') {
+            if (s.charAt(i) == '1') {
                 c++;
-            } else {
-                if (c > 0) {
-                    ls.add(c);
-                    c = 0;
-                }
+            } else if (c > 0) {
+                ans = (ans + (long) c * (c + 1) % mod * inv2) % mod;
+                c = 0;
             }
         }
         if (c > 0) {
-            ls.add(c);
-            c = 0;
+            ans = (ans + (long) c * (c + 1) % mod * inv2) % mod;
         }
-        int mod = 1000000007;
-        int ans = 0;
-        for (int i : ls) {
-            long term1 = i % mod;
-            long term2 = (i + 1) % mod;
-            long product = (term1 * term2) % mod;
-
-            ans = (int)(ans + (product * ((mod + 1) / 2)) % mod) % mod;
-        }
-        return ans;
+        return (int) ans;
     }
 }
