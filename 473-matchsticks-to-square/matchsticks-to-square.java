939cde45f52552587c17ea582781664ddc958a71
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-16 19:55:44 +0530

    Time: 180 ms (11.45%) | Memory: 43.4 MB (26.63%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.java
+++ 473-matchsticks-to-square/matchsticks-to-square.java
@@ -3,38 +3,42 @@ class Solution {
         if (sum1 == 0 && sum2 == 0 && sum3 == 0 && sum4 == 0) {
             return true;
         }
-        if (sum1 < 0 || sum2 < 0 || sum3 < 0 || sum4 < 0) {
+        if(sum1<0 || sum2<0 || sum3<0 || sum4<0)
+        {
             return false;
         }
-        if (i >= nums.length) {
+        if(i>=nums.length)
+        {
             return false;
         }
-
         sum1 -= nums[i];
         boolean check1 = check(nums, i + 1, sum1, sum2, sum3, sum4);
+        if(check1)
+        {
+            return true;
+        }
         sum1 += nums[i];
-        if (check1) return true;
-
-        if (sum2 != sum1) {
-            sum2 -= nums[i];
-            boolean check2 = check(nums, i + 1, sum1, sum2, sum3, sum4);
-            sum2 += nums[i];
-            if (check2) return true;
+        sum2 -= nums[i];
+        boolean check2 = check(nums, i + 1, sum1, sum2, sum3, sum4);
+        if(check2)
+        {
+            return true;
         }
-
-        if (sum3 != sum2) {
-            sum3 -= nums[i];
-            boolean check3 = check(nums, i + 1, sum1, sum2, sum3, sum4);
-            sum3 += nums[i];
-            if (check3) return true;
+        sum2 += nums[i];
+        sum3 -= nums[i];
+        boolean check3 = check(nums, i + 1, sum1, sum2, sum3, sum4);
+        if(check3)
+        {
+            return true;
         }
-
-        if (sum4 != sum3) {
-            sum4 -= nums[i];
-            boolean check4 = check(nums, i + 1, sum1, sum2, sum3, sum4);
-            sum4 += nums[i];
-            if (check4) return true;
+        sum3 += nums[i];
+        sum4 -= nums[i];
+        boolean check4 = check(nums, i + 1, sum1, sum2, sum3, sum4);
+        if(check4)
+        {
+            return true;
         }
+        sum4 += nums[i];
 
         return false;
     }
@@ -42,24 +46,26 @@ class Solution {
     public boolean makesquare(int[] nums) {
         int n = nums.length;
         int sum = 0;
-        int max = 0;
         for (int i : nums) {
             sum += i;
-            max = Math.max(max, i);
         }
         if (sum % 4 != 0) {
             return false;
         }
-        int side = sum / 4;
-        if (max > side) {
-            return false;
+        for(int i : nums)
+        {
+            if(i>sum/4)
+            {
+                return false;
+            }
         }
-
-        Integer[] boxed = new Integer[n];
-        for (int i = 0; i < n; i++) boxed[i] = nums[i];
-        Arrays.sort(boxed, Collections.reverseOrder());
-        for (int i = 0; i < n; i++) nums[i] = boxed[i];
-
-        return check(nums, 0, side, side, side, side);
+        Arrays.sort(nums);
+        for(int i=0;i<n/2;i++)
+        {
+            int x=nums[i];
+            nums[i]=nums[n-i-1];
+            nums[n-i-1]=x;
+        }
+        return check(nums, 0, sum / 4, sum / 4, sum / 4, sum / 4);
     }
 }
