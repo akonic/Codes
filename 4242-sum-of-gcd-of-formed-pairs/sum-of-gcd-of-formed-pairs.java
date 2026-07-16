82930a6036ccec9d250f2ce2e1507a48bc6e98d4
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-16 18:34:33 +0530

    Time: 53 ms (95.50%) | Memory: 107.9 MB (73.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,28 +1,29 @@
 class Solution {
-private long gcd(long a, long b) {
-    while (b != 0) {
-        long t = a % b;
-        a = b;
-        b = t;
-    }
-    return a;
-}
-    public long gcdSum(int[] nums) {
-        int n=nums.length;
-        int[] preMax= new int[n];
-        long[] preGcd = new long[n];
-        int x=nums[0];
-        for(int i=0;i<n;i++)
-        {
-            x=Math.max(x,nums[i]);
-            preMax[i]=x;
-            preGcd[i]=gcd(x,nums[i]);
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
         }
-        long ans=0;
+        return a;
+    }
+
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] preGcd = new int[n];
+        int x = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > x)
+                x = nums[i];
+            preGcd[i] = gcd(x, nums[i]);
+        }
+
         Arrays.sort(preGcd);
-        for(int i=0;i<n/2;i++)
-        {
-            ans+=gcd(preGcd[i],preGcd[n-i-1]);
+
+        long ans = 0;
+        for (int i = 0; i < n / 2; i++) {
+            ans += gcd(preGcd[i], preGcd[n - i - 1]);
         }
         return ans;
     }
