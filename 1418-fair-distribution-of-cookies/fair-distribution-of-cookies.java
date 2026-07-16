f929c6860bb6608e21cd405b9bcc5edfe4d5d97f
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-16 23:28:05 +0530

    Time: 365 ms (31.08%) | Memory: 42.4 MB (85.41%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int maxOfArray(int[] dist) {
+        int ans = Integer.MIN_VALUE;
+        for (int i : dist) {
+            ans = Math.max(ans, i);
+        }
+        return ans;
+    }
+
+    private int check(int[] cookies, int i, int[] dist, int l) {
+        int n = cookies.length;
+        if (i >= cookies.length) {
+            return maxOfArray(dist);
+        }
+        int ans = Integer.MAX_VALUE;
+
+        for (int k = 0; k < l; k++) {
+            dist[k] += cookies[i];
+            ans = Math.min(ans, check(cookies, i + 1, dist, l));
+            dist[k] -= cookies[i];
+        }
+
+        return ans;
+    }
+
+    public int distributeCookies(int[] cookies, int k) {
+        int n = cookies.length;
+        int[] dist = new int[k];
+        Arrays.fill(dist, 0);
+        return check(cookies, 0, dist, k);
+    }
+}
