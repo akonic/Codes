be9ace6bb45dd2a787ee04e969406c6418a2a849
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-18 22:26:14 +0530

    Time: 17 ms (86.25%) | Memory: 52 MB (8.77%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -2,29 +2,37 @@ class Solution {
     public int maximalRectangle(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]=='1')
-                {
-                    int len=Integer.MAX_VALUE;;
-                    for(int k=i;k<n;k++)
-                    {
-                        int l=j;
-                        while(l<m && matrix[k][l]=='1')
-                        {
-                            l++;
-                        }
-                        len=Math.min(len,l-j);
-                        ans=Math.max(ans,len*(k-i+1));
-
-                    }    
+        int[][] depth = new int[n][m];
+       
+        int ans = 0;
+       
+        for (int j = 0; j < m; j++) {
+            int c = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (matrix[i][j] == '1') {
+                    c++;
+                } else {
+                    c = 0;
+                }
+                depth[i][j] = c;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    int x = depth[i][j];
+                    int width = 0;
+                    int k = j;
+                    while (k < m && matrix[i][k] == '1') {
+                        x = Math.min(x, depth[i][k]);
+                        width++;
+                        ans = Math.max(ans, x * width);
+                        k++;
+                    }
                 }
             }
         }
+
         return ans;
     }
 }
