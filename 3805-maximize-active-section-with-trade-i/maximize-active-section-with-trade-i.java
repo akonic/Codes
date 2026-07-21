8a3110ce3c3a7323571816c2c6a1c072ce5c4fb6
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-21 23:38:40 +0530

    Time: 67 ms (74.65%) | Memory: 47.7 MB (94.37%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        List<Integer> ls = new ArrayList<>();
+        int n = s.length();
+        int count=0,c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='0')
+            {
+                count++;
+            }
+            else{
+                c++;
+                if(count>0){
+                ls.add(count);}
+                count=0;
+            }
+        }
+        if(count>0)
+        {
+            ls.add(count);
+        }
+        int ans=0;
+        int x=ls.size();
+        for(int i=1;i<x;i++)
+        {
+            int temp=ls.get(i-1)+ls.get(i);
+            ans=Math.max(ans,temp);
+        }
+
+        
+
+
+        return ans+c; 
+    }
+}
