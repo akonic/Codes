4f6c4532ce5700cf8bad8e25aeaa4ec5878bccce
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-23 19:11:33 +0530

    Time: 47 ms (10.16%) | Memory: 46 MB (11.05%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public int integerReplacement(int n) {
+        int ans=0;
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b)->Long.compare(a[0], b[0]));
+        pq.offer(new long[]{0,n});
+        while(!pq.isEmpty())
+        {
+            long[] top = pq.poll();
+            long num = top[1];
+            long cost = top[0];
+            if(num==1)
+            {
+                return (int)cost;
+            }
+            if(num%2==0)
+            {
+                pq.offer(new long[]{cost+1,num/2});
+            }
+            else{
+                pq.offer(new long[]{cost+1,num-1});
+                if(num<=Integer.MAX_VALUE){
+                pq.offer(new long[]{cost+1,num+1});}
+            }
+        }
+        return ans;
+    }
+}
