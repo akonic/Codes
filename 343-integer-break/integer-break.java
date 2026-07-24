59333276f6cb577f481a4c038103e964fa128215
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-24 13:15:20 +0530

    Time: 20 ms (3.51%) | Memory: 46.6 MB (6.99%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,28 +1,4 @@
 class Solution {
-    private int check(int[] nums)
-    {
-        int count=0,prod=0;
-        for(int i=0;i<nums.length;i++)
-        {   int pro=0;
-            if(nums[i]>=0)
-            {
-                count++;
-                pro=1;
-                int x=nums[i];
-                while(x>0)
-                {
-                    pro*=nums[i];
-                    x--;
-                }
-            }
-            prod+=pro;
-        }
-        if(count>=2)
-        {
-            return prod;
-        }
-        return -1;
-    }
     private int helper(int n,int i,int count,int original,int[][][] dp)
     {   //System.out.println(n);
         if(n==0 && count>=2)
@@ -37,8 +13,9 @@ class Solution {
         {
             return dp[n][count][i];
         }
-        int pick = i*helper(n-i,i,count+1,original,dp);
-        int notPick = helper(n,i+1,count,original,dp);
+        int pick=0,notPick=0;
+         pick+= i*helper(n-i,i,count+1,original,dp);
+         notPick+= helper(n,i+1,count,original,dp);
 
         return dp[n][count][i] = Math.max(pick,notPick);
     }
