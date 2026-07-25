16b3d5c411b4cbf87a806e6ee6a98ea02f66573e
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-25 23:23:26 +0530

    Time: 3 ms (74.28%) | Memory: 43.4 MB (39.84%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,31 +1,27 @@
 class Solution {
-    private int helper(char[] a,char[] b,char[] c,int i,int j,int k,int[][][] dp)
+    private int helper(char[] a,char[] b,char[] c,int i,int j,int[][] dp)
     {
         int n=a.length;
         int m=b.length;
         int o=c.length;
-        if(k==o && i==n && j==m)
+        if(i==n && j==m)
         {
             return 1;
         }
-        if(dp[i][j][k]!=-1)
+        if(dp[i][j]!=-1)
         {
-            return dp[i][j][k];
+            return dp[i][j];
         }
-        // if((i==n && j!=m && k!=o) || (j==m && i!=n && k!=o) || (k==o && i!=n && j!=m))
-        // {
-        //     return false;
-        // }
-        int a1= 0,b1=0;
+        int a1= 0,b1=0,k=i+j;
         if(i<n && k<o && a[i]==c[k])
         {
-            a1=helper(a,b,c,i+1,j,k+1,dp);
+            a1=helper(a,b,c,i+1,j,dp);
         }
         if(j<m && k<o  && b[j]==c[k])
         {
-            b1=helper(a,b,c,i,j+1,k+1,dp);
+            b1=helper(a,b,c,i,j+1,dp);
         }
-        return dp[i][j][k] = Math.max(a1,b1);
+        return dp[i][j] = Math.max(a1,b1);
     }
     public boolean isInterleave(String s1, String s2, String s3) {
         char[] a = s1.toCharArray();
@@ -34,15 +30,16 @@ class Solution {
         int n=a.length;
         int m=b.length;
         int o=c.length;
-        int[][][] dp = new int[n+1][m+1][o+1];
-        for(int[][] i : dp)
+        if(n+m!=o)
         {
-            for(int[] j : i)
-            {
-                Arrays.fill(j,-1);
-            }
+            return false;
         }
-        int x= helper(a,b,c,0,0,0,dp);
+        int[][] dp = new int[n+1][m+1];
+        for(int[] i : dp)
+        {
+             Arrays.fill(i,-1);
+        }
+        int x= helper(a,b,c,0,0,dp);
         if(x==0)
         {
             return false;
