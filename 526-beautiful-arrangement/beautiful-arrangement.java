2b85bdd360cde15862515301ec66b7a627080966
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-26 20:12:40 +0530

    Time: 43 ms (34.08%) | Memory: 41.7 MB (94.69%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -1,49 +1,26 @@
 class Solution {
     int ans;
-    private boolean check(List<Integer> ls)
+    public void shuffle(boolean[] visited,int count,int n)
     {
-        int n=ls.size();
-        for(int i=0;i<n;i++)
+        if(count==n+1)
         {
-            int x=ls.get(i);
-            if(x%(i+1)==0 || (i+1)%x==0)
-            {
-
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
-    }
-    public void shuffle(List<Integer> ls,boolean[] visited,int count,int n)
-    {
-        if(count==n)
-        {
-            
-                ans++;
+            ans++;
             return;
         }
         for(int i=1;i<=n;i++)
         {
-            if(!visited[i] )
+            if(!visited[i] && (i%count==0 || count%i==0))
             {
-                visited[i]=true;
-                ls.add(i);
-                if(check(ls)) 
-                {
-                    shuffle(ls,visited,count+1,n);
-                }
-                ls.remove(ls.size()-1);
+                visited[i]=true;    
+                shuffle(visited,count+1,n);
                 visited[i]=false;
             }
         }
     }
     public int countArrangement(int n) {
         ans=0;
-        List<Integer> ls = new ArrayList<>();
         boolean[] visited = new boolean[n+1];
-        shuffle(ls,visited,0,n);
+        shuffle(visited,1,n);
         return ans;
     }
 }
