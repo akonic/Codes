7b5cec7848b21e3bd58896da158bc1a21e9e01fd
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-07-29 23:14:08 +0530

    Time: 42 ms (7.67%) | Memory: 47.3 MB (6.17%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    private void helper(Set<List<Integer>> st, List<Integer> ls, int[] nums,boolean[] vis) {
+    private void helper(Set<List<Integer>> st, List<Integer> ls, int[] nums, boolean[] vis) {
         if (ls.size() == vis.length) {
             st.add(new ArrayList<>(ls));
             return;
@@ -8,7 +8,7 @@ class Solution {
             if (vis[i] == false) {
                 vis[i] = true;
                 ls.add(nums[i]);
-                helper(st,ls,nums,vis);
+                helper(st, ls, nums, vis);
                 ls.remove(ls.size() - 1);
                 vis[i] = false;
             }
@@ -16,17 +16,24 @@ class Solution {
     }
 
     public List<List<Integer>> permuteUnique(int[] nums) {
-
-        Set<List<Integer>> st = new HashSet<>();
         int n = nums.length;
+        if (n == 1) {
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums[0]);
+            ans.add(list);
+            return ans;
+        }
+        Set<List<Integer>> st = new HashSet<>();
+
         boolean[] vis = new boolean[n];
         List<Integer> ls = new ArrayList<>();
-        helper(st,ls,nums,vis);
-        List<List<Integer>> ans=new ArrayList<>();
-        for(List<Integer> temp : st)
-        {
-             ans.add(new ArrayList<>(temp));
+        helper(st, ls, nums, vis);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (List<Integer> temp : st) {
+            ans.add(new ArrayList<>(temp));
         }
+        st.clear();
         return ans;
     }
 }
