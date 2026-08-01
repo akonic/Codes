f8b8653f1460ef6a0d1db7343096987457f413f0
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-01 19:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (57.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,5 +1,5 @@
 class Solution {
-    private int helper(int[] nums,int i,int j)
+    private int helper(int[] nums,int i,int j,int[][] dp)
     {
         if(i>j)
         {
@@ -9,10 +9,14 @@ class Solution {
         {
             return nums[i];
         }
-        int take_i = nums[i]+Math.min(helper(nums,i+2,j),helper(nums,i+1,j-1));
-        int take_j = nums[j]+Math.min(helper(nums,i+1,j-1),helper(nums,i,j-2));
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int take_i = nums[i]+Math.min(helper(nums,i+2,j,dp),helper(nums,i+1,j-1,dp));
+        int take_j = nums[j]+Math.min(helper(nums,i+1,j-1,dp),helper(nums,i,j-2,dp));
 
-        return Math.max(take_i,take_j);
+        return dp[i][j]=Math.max(take_i,take_j);
     }
     public boolean predictTheWinner(int[] nums) {
         int n=nums.length;
@@ -21,7 +25,12 @@ class Solution {
         {
             sum+=i;
         }
-        int score = helper(nums,0,n-1);
+        int[][] dp = new int[n][n];
+        for(int[] i : dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        int score = helper(nums,0,n-1,dp);
         if(score>=sum-score)
         {
             return true;
