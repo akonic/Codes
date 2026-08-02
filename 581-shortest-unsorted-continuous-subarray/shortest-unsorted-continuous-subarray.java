a8adb8701516990fe44f96a12ca6f6d29a575f2a
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-02 17:13:45 +0530

    Time: 1 ms (89.11%) | Memory: 47.4 MB (32.95%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -1,14 +1,22 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int n = nums.length;
-        int[] copy = new int[n];
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        int x=Integer.MIN_VALUE;
         for(int i=0;i<n;i++)
         {
-            copy[i]=nums[i];
+            x=Math.max(x,nums[i]);
+            pre[i]=x;
         }
-        Arrays.sort(copy);
+        x=Integer.MAX_VALUE;
+        for(int j=n-1;j>=0;j--)
+        {
+            x=Math.min(x,nums[j]);
+            suff[j]=x;
+        }
         int i=0,j=n-1;
-        while(i<n && copy[i]==nums[i])
+        while(i<n && pre[i]==suff[i])
         {
             i++;
         }
@@ -16,7 +24,7 @@ class Solution {
         {
             return 0;
         }
-        while(j>=0 && copy[j]==nums[j])
+        while(j>=0 && pre[j]==suff[j])
         {
             j--;
         }
