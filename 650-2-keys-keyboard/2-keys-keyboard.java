f21b964f1a8f1c87f7c99200c679c459f27c31cf
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-04 08:21:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (77.40%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,39 +1,31 @@
 class Solution {
-    private int helper(int n,int i,int prev,int picked,int[][][] dp)
+    private int sum(int n)
     {
-        if(n==i)
+        int ans=0;
+        for (int i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                ans+=i;
+                n /= i;
+            }
+        }
+        if(n>2)
         {
-            return 0;
+            ans+=n;
         }
-        if(i>n || prev>n)
-        {
-            return 1000;
-        }
-        if(dp[i][prev][picked]!=-1)
-        {
-            return dp[i][prev][picked];
-        }
-        int a = 1+helper(n,i+prev,prev,0,dp);
-        int b=1000;
-        if(picked==0){
-         b= 1+ helper(n,i,i,1,dp);
-        }
-        return dp[i][prev][picked]=Math.min(a,b);
+        return ans;
     }
     public int minSteps(int n) {
         if(n==1)
         {
             return 0;
         }
-        int[][][] dp = new int[n+1][n+1][2];
-        for(int[][] i : dp)
+        int ans=0;
+        while(n%2==0)
         {
-            for(int[] j : i)
-            {
-                Arrays.fill(j,-1);
-            }
+            n/=2;
+            ans+=2;
         }
-        return 1 + helper(n,1,1,1,dp);
-
+        ans+=sum(n);
+        return ans;
     }
 }
