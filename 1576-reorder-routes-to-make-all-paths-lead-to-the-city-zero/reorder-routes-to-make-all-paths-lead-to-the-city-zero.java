4921390dc163af0af89f9312cc2880a492315ca1
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-05 22:17:17 +0530

    Time: 39 ms (58.09%) | Memory: 108.5 MB (96.33%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,32 +1,26 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
         List<List<int[]>> ls = new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             ls.add(new ArrayList<>());
         }
-        for(int[] i : connections)
-        {
-            ls.get(i[0]).add(new int[]{i[1],1});
-            ls.get(i[1]).add(new int[]{i[0],0});
+        for (int[] i : connections) {
+            ls.get(i[0]).add(new int[] { i[1], 1 });
+            ls.get(i[1]).add(new int[] { i[0], 0 });
         }
         boolean[] reachable = new boolean[n];
         Queue<Integer> q = new ArrayDeque<>();
-        reachable[0]=true;
-        int ans=0;
+        reachable[0] = true;
+        int ans = 0;
         q.offer(0);
-        while(!q.isEmpty())
-        {
+        while (!q.isEmpty()) {
             int p = q.poll();
-            for(int[] i : ls.get(p))
-            {
-                if(!reachable[i[0]])
-                {
-                    if(i[1]==1)
-                    {
-                        ans++;
-                    }
-                    reachable[i[0]]=true;
+            for (int[] i : ls.get(p)) {
+                if (!reachable[i[0]]) {
+
+                    ans += i[1];
+
+                    reachable[i[0]] = true;
                     q.offer(i[0]);
                 }
             }
