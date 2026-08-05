bb323fa3f966c5c4640285e5d73b0f5da2506bfa
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-05 22:12:26 +0530

    Time: 39 ms (58.09%) | Memory: 115.1 MB (60.63%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        List<List<int[]>> ls = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            ls.add(new ArrayList<>());
+        }
+        for(int[] i : connections)
+        {
+            ls.get(i[0]).add(new int[]{i[1],1});
+            ls.get(i[1]).add(new int[]{i[0],0});
+        }
+        boolean[] reachable = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        reachable[0]=true;
+        int ans=0;
+        for(int[] i : ls.get(0))
+        {
+            if(i[1]==1)
+            {
+                ans++;
+            }
+            q.offer(i[0]);
+            reachable[i[0]]=true;
+        }
+        while(!q.isEmpty())
+        {
+            int p = q.poll();
+            for(int[] i : ls.get(p))
+            {
+                if(!reachable[i[0]])
+                {
+                    if(i[1]==1)
+                    {
+                        ans++;
+                    }
+                    reachable[i[0]]=true;
+                    q.offer(i[0]);
+                }
+            }
+        }
+        return ans;
+    }
+}
