16997d2bf890096e10726857b9e42bc2c5b7af68
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-05 17:26:32 +0530

    Time: 70 ms (62.50%) | Memory: 278 MB (84.62%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -1,46 +1,38 @@
 class Solution {
-    private void dfs(List<List<Integer>> ls,boolean[] infected, int i)
-    {
-        infected[i]=true;
-        for(int j : ls.get(i))
-        {
-            if(!infected[j])
-            {
-                dfs(ls,infected,j);
+    private void dfs(List<List<Integer>> ls, boolean[] infected, int i) {
+        infected[i] = true;
+        for (int j : ls.get(i)) {
+            if (!infected[j]) {
+                dfs(ls, infected, j);
             }
         }
 
     }
+
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
-        List<Integer> ans = new ArrayList<>();
-        List<Integer> ans1 = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>(n);
         List<List<Integer>> ls = new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {   ans1.add(i);
+        for (int i = 0; i < n; i++) {
             ls.add(new ArrayList<>());
         }
         int m = invocations.length;
-        for(int[] i : invocations)
-        {
+        for (int[] i : invocations) {
             ls.get(i[0]).add(i[1]);
         }
         boolean[] infected = new boolean[n];
-        dfs(ls,infected,k);
-        for(int i=0;i<n;i++)
-        {
-            List<Integer> temp = ls.get(i);
-            for (int j : temp)
-            {
-                if(infected[j] && !infected[i])
-                {
-                    return ans1;
+        dfs(ls, infected, k);
+        for (int i = 0; i < n; i++) {
+            for (int j : ls.get(i)) {
+                if (infected[j] && !infected[i]) {
+                    List<Integer> all = new ArrayList<>(n);
+                    for (int x = 0; x < n; x++)
+                        all.add(x);
+                    return all;
                 }
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(!infected[i])
-            {
+        for (int i = 0; i < n; i++) {
+            if (!infected[i]) {
                 ans.add(i);
             }
         }
