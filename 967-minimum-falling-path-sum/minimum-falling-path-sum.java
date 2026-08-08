c17028dffb82c85fdb28885381e311b9ae6cd5f9
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-09 01:09:27 +0530

    Time: 8 ms (27.63%) | Memory: 47.3 MB (30.13%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,29 +1,27 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int ans=Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
         int n = matrix.length;
-       
-        for(int i=1;i<n;i++)
-        {   
-           for (int j=0;j<n;j++)
-           {
-                int p=matrix[i-1][j];
-                if(j-1>=0)
-                {
-                    p=Math.min(p,matrix[i-1][j-1]);
-                }
-                if(j+1<n)
-                {
-                    p=Math.min(p,matrix[i-1][j+1]);
-                }
 
-                matrix[i][j]+=p;
-           }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    int p = matrix[i - 1][j];
+                    if (j - 1 >= 0) {
+                        p = Math.min(p, matrix[i - 1][j - 1]);
+                    }
+                    if (j + 1 < n) {
+                        p = Math.min(p, matrix[i - 1][j + 1]);
+                    }
+
+                    matrix[i][j] += p;
+                }
+                if (i == n - 1) {
+                    ans = Math.min(ans, matrix[n - 1][j]);
+                }
+            }
         }
-        for(int i=0;i<n;i++)
-        { 
-            ans=Math.min(ans,matrix[n-1][i]);
-        }
+
         return ans;
     }
 }
