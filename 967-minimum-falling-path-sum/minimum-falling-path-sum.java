9731ffcd9bd9ef501fb9615682d7bba04b2d985f
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-09 01:08:08 +0530

    Time: 6 ms (59.87%) | Memory: 47.5 MB (17.63%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,30 +1,8 @@
 class Solution {
-    private int helper(int[][] matrix,int i,int j,int[][] dp)
-    {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        if(i==n)
-        {
-            return 0;
-        }
-        if(j<0 || j>=m)
-        {
-            return 100000;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        return dp[i][j]= matrix[i][j]  + Math.min(helper(matrix,i+1,j,dp),Math.min(helper(matrix,i+1,j-1,dp),helper(matrix,i+1,j+1,dp)));
-    }
     public int minFallingPathSum(int[][] matrix) {
         int ans=Integer.MAX_VALUE;
         int n = matrix.length;
-        int[][] dp = new int[n][n];
-            for(int[] j : dp)
-            {
-                Arrays.fill(j,-1);
-            }
+       
         for(int i=1;i<n;i++)
         {   
            for (int j=0;j<n;j++)
