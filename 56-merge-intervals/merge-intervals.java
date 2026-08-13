5a2229f2545fd69c5edbd237af6325b9c2eb00f0
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-13 10:32:03 +0530

    Time: 8 ms (91.73%) | Memory: 48.5 MB (97.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         int n = intervals.length;
-        int[][] ans = new int[n][2];
-        int idx = 0;
-        int x = intervals[0][0];
-        int y = intervals[0][1];
-        for (int i = 1; i < n; i++) {
-            int a = intervals[i][0];
-            int b = intervals[i][1];
-            if (y >= a && y >= b) {
-
-            } else if (y >= a && y < b) {
-                y = b;
-            } else if (y < a) {
-                ans[idx][0] = x;
-                ans[idx][1] = y;
-                idx++;
-                x = a;
-                y = b;
+        List<int[]> ls = new ArrayList<>();
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        int x=intervals[0][0];
+        int y=intervals[0][1];
+        for(int i=1;i<n;i++)
+        {
+            if(y<intervals[i][0])
+            {
+                ls.add(new int[]{x,y});
+                x=intervals[i][0];
+                y=intervals[i][1];
+            }
+            else{
+                y=Math.max(y,intervals[i][1]);
             }
         }
-        ans[idx][0] = x;
-        ans[idx][1] = y;
-        idx++;
+        ls.add(new int[]{x,y});
 
-        return Arrays.copyOf(ans, idx);
+        int m = ls.size();
+        int[][] ans = new int[m][2];
+        for(int i=0;i<m;i++)
+        {
+            ans[i][0]=ls.get(i)[0];
+            ans[i][1]=ls.get(i)[1];
+        }
+        return ans;
     }
 }
