f5101986bd1d53e15b8684fb2706452e632982cc
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-17 03:58:05 +0530

    Time: 154 ms (23.97%) | Memory: 94.4 MB (5.34%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     private boolean check(char[] ch, int j, int len) {
-        int i = j - len+1;
+        int i = j - len + 1;
         while (i < j) {
             if (ch[i] == ch[j]) {
                 i++;
@@ -21,33 +21,24 @@ class Solution {
         }
         int[][] dp = new int[n + 1][n + 1];
         int ans = 0, x = -1, y = -1;
+        int maxLen = 1;
+        int end = 0;
         List<int[]> ls = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (ch[i - 1] == bh[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
-                if (dp[i][j] > 1) {
-                    ls.add(new int[] { dp[i][j], i });
-                }
-            }
+                int len = dp[i][j];
+                int originalEnd = i - 1;
 
-        }
-        StringBuilder t = new StringBuilder();
-        Collections.sort(ls, (a, b) -> Integer.compare(b[0], a[0]));
-        for (int[] i : ls) {
-            if (check(ch, i[1] - 1, i[0])) {
-                int u = i[0];
-                int j = i[1] - 1;
-                while (u > 0) {
-                    u--;
-                    t.append(ch[j]);
-                    j--;
+                if (len > maxLen && check(ch, originalEnd, len)) {
+                    maxLen = len;
+                    end = originalEnd;
                 }
-                return t.toString();
             }
         }
-        t.append(s.charAt(0));
-        return t.toString();
+
+        return s.substring(end - maxLen + 1, end + 1);
     }
 }
