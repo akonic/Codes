601584adb2e0ae5fadcd8dcb14eb146d64fae733
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-18 05:16:48 +0530

    Time: 2 ms (70.01%) | Memory: 44.1 MB (92.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,50 +1,50 @@
 class Solution {
-    private boolean check(char[] ch,int n)
-    {
-       int c=0;
-        for(int i=0;i<n;i++)
-        {
-            if(ch[i]=='(')
-            {
+    private boolean check(char[] ch, int n) {
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            if (ch[i] == '(') {
                 c++;
-            }
-            else{
-                if(c>0)
-                {
+            } else {
+                if (c > 0) {
                     c--;
-                }
-                else{
+                } else {
                     return false;
                 }
             }
         }
-        if(c!=0)
-        {
+        if (c != 0) {
             return false;
         }
         return true;
     }
-    private void helper(List<String> ls,char[] ch,int n,int i)
-    {
-        if(i==n)
-        {
-            if(check(ch,n))
-            {
+
+    private void helper(List<String> ls, char[] ch, int n, int i) {
+        if (i == n) {
+            if (check(ch, n)) {
                 ls.add(String.valueOf(ch));
             }
             return;
         }
+        if (ch[i - 1] == '(') {
+            ch[i] = '(';
+            helper(ls, ch, n, i + 1);
+            ch[i] = ')';
+            helper(ls, ch, n, i + 1);
+        } else {
+            ch[i] = ')';
+            helper(ls, ch, n, i + 1);
+            ch[i] = '(';
+            helper(ls, ch, n, i + 1);
+        }
 
-        ch[i]='(';
-        helper(ls,ch,n,i+1);
-        ch[i]=')';
-        helper(ls,ch,n,i+1);
     }
+
     public List<String> generateParenthesis(int n) {
-        char[] ch = new char[2*n];
+        char[] ch = new char[2 * n];
 
         List<String> ls = new ArrayList<>();
-        helper(ls,ch,2*n,0);
+        ch[0] = '(';
+        helper(ls, ch, 2 * n, 1);
 
         return ls;
     }
