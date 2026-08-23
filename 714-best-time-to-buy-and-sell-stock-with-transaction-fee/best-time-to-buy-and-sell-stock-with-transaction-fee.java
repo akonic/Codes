e24f0b2f2f00524c3f69f9d5c9483245055ecfe7
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-08-23 10:52:51 +0530

    Time: 19 ms (42.13%) | Memory: 70.5 MB (50.85%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,32 +1,13 @@
 class Solution {
 
-    private int helper(int[] prices, int fee, int i, int flag, int[][] dp) {
-        if (i == prices.length) {
-            return 0;
-        }
-
-        if (dp[i][flag] != -1) {
-            return dp[i][flag];
-        }
-
-        int skip = helper(prices, fee, i + 1, flag, dp);
-        int pick;
-
-        if (flag == 0) {
-            pick = helper(prices, fee, i + 1, 1, dp) - prices[i];
-        } else {
-            pick = prices[i] - fee + helper(prices, fee, i + 1, 0, dp);
-        }
-
-        return dp[i][flag] = Math.max(pick, skip);
-    }
-
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp = new int[prices.length][2];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
 
-        return helper(prices, fee, 0, 0, dp);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][0] = Math.max(dp[i + 1][0], dp[i + 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i + 1][1], dp[i + 1][0] + prices[i] - fee);
+        }
+        return dp[0][0];
     }
 }
