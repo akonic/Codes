052ae952f16b92b446458641841a2f49ea330495
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-02 21:24:34 +0530

    Time: 49 ms (100.00%) | Memory: 54.2 MB (62.50%) - LeetSync

--- 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.java
+++ 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.java
@@ -1,38 +1,20 @@
 class Solution {
     public boolean isPossibleToRearrange(String s, String t, int k) {
-        Map<String, Integer> mp = new HashMap<>();
-        char[] ch = s.toCharArray();
-        char[] bh = t.toCharArray();
-        int n = ch.length;
-        int p = n / k;
-        StringBuilder temp = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            temp.append(ch[i]);
-            if (temp.length() == p) {
-                String u=temp.toString();
-                mp.put(u, mp.getOrDefault(u, 0) + 1);
-                temp = new StringBuilder();
-            }
+        int p = s.length() / k;
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i += p) {
+            String part = s.substring(i, i + p);
+            map.put(part, map.getOrDefault(part, 0) + 1);
         }
-        temp = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            temp.append(bh[i]);
-            if (temp.length() == p) {
-                String u=temp.toString();
-                if (mp.containsKey(u)) {
-                    if (mp.get(u) > 0) {
-                        mp.put(u, mp.getOrDefault(u, 0) - 1);
-                        temp = new StringBuilder();
-                    } else {
-                        return false;
-                    }
-                }
-                else{
-                    return false;
-                }
-                temp = new StringBuilder();
-            }
+
+        for (int i = 0; i < t.length(); i += p) {
+            String part = t.substring(i, i + p);
+            Integer cnt = map.get(part);
+            if (cnt == null || cnt == 0) return false;
+            map.put(part, cnt - 1);
         }
+
         return true;
     }
 }
