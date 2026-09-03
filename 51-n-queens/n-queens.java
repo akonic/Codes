49ab322be5646842f40678321d57f8567d5c3827
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-03 23:44:28 +0530

    Time: 7 ms (11.24%) | Memory: 46.3 MB (91.74%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,118 +1,114 @@
 class Solution {
     Set<List<String>> ans = new HashSet<>();
-    private void build(boolean[][] row)
-    {
-       
+
+    private void build(boolean[][] row) {
+
         List<String> ls = new ArrayList<>();
-        for(int i=0;i<row.length;i++)
-        {
+        for (int i = 0; i < row.length; i++) {
             StringBuilder s = new StringBuilder();
-            for(int j=0;j<row.length;j++)
-            {
-                if(row[i][j])
-                {
+            for (int j = 0; j < row.length; j++) {
+                if (row[i][j]) {
                     s.append('Q');
-                }
-                else{
+                } else {
                     s.append('.');
                 }
             }
             ls.add(s.toString());
-        }   
+        }
         ans.add(new ArrayList(ls));
     }
-    private boolean check(boolean[][] row,int i,int j)
-    {
-        
-            for(int b=0;b<row.length;b++)
-            {
-                if(row[i][b])
-                {
-                    return false;
-                }
-            }
-            for(int b=0;b<row.length;b++)
-            {
-                if(row[b][j])
-                {
-                    return false;
-                }
-            }
-            int x=i,y=j;
-            while(x>=0 && y>=0)
-            {
-                if(row[x][y])
-                {
-                    return false;
-                }
-                x--;y--;
-            }
-            x=i;y=j;
-            while(x<row.length && y<row.length)
-            {
-                if(row[x][y])
-                {
-                    return false;
-                }
-                x++;y++;
-            } x=i;y=j;
-            while(x>=0 && y<row.length)
-            {
-                if(row[x][y])
-                {
-                    return false;
-                }
-                x--;y++;
-            } x=i;y=j;
-            while(x<row.length && y>=0)
-            {
-                if(row[x][y])
-                {
-                    return false;
-                }
-                x++;y--;
-            }
-            return true;
-        
-    }
-    private void helper(int n,int i,int j,boolean[][] row)
-    {
 
-        if(n==0)
-        {
+    private boolean check(boolean[][] row, int i, int j) {
+
+        for (int b = 0; b < row.length; b++) {
+            if (row[i][b]) {
+                return false;
+            }
+        }
+        for (int b = 0; b < row.length; b++) {
+            if (row[b][j]) {
+                return false;
+            }
+        }
+        int x = i, y = j;
+        while (x >= 0 && y >= 0) {
+            if (row[x][y]) {
+                return false;
+            }
+            x--;
+            y--;
+        }
+        x = i;
+        y = j;
+        while (x < row.length && y < row.length) {
+            if (row[x][y]) {
+                return false;
+            }
+            x++;
+            y++;
+        }
+        x = i;
+        y = j;
+        while (x >= 0 && y < row.length) {
+            if (row[x][y]) {
+                return false;
+            }
+            x--;
+            y++;
+        }
+        x = i;
+        y = j;
+        while (x < row.length && y >= 0) {
+            if (row[x][y]) {
+                return false;
+            }
+            x++;
+            y--;
+        }
+        return true;
+
+    }
+
+    private void helper(int n, int i, int j, boolean[][] row) {
+
+        if (n == 0) {
             build(row);
             return;
         }
-        if(i>=row.length || j>=row.length)
-        {
+        if (i >= row.length || j >= row.length) {
             return;
         }
-        if(check(row,i,j))
-        {
-            row[i][j]=true;
-            for(int a=0;a<row.length;a++){
-            helper(n-1,i+1,a,row);
+        if (check(row, i, j)) {
+            row[i][j] = true;
+            if (i == row.length - 1) {
+                helper(n - 1, i + 1, 0, row);
+            } else {
+                for (int a = 0; a < row.length; a++) {
+                    if (check(row, i + 1, a)) {
+                        helper(n - 1, i + 1, a, row);
+                    }
+                }
             }
-            row[i][j]=false;
-            
+            row[i][j] = false;
+
         }
-        return ;
-        
+        return;
+
     }
+
     public List<List<String>> solveNQueens(int n) {
         ans.clear();
-        for(int i=0;i<n;i++)
-        {   boolean[][] row = new boolean[n][n];
-           
-            helper(n,0,i,row);
-           
-        }  
+        for (int i = 0; i < n; i++) {
+            boolean[][] row = new boolean[n][n];
+
+            helper(n, 0, i, row);
+
+        }
         List<List<String>> p = new ArrayList<>();
-        for(List<String> u : ans)
-        {
+        for (List<String> u : ans) {
             p.add(new ArrayList<>(u));
         }
 
-        return p;  
+        return p;
     }
 }
