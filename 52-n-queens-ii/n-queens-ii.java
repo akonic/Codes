be69ef2667a1d3aec423aa6833d110cbfeca06ac
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-03 23:46:47 +0530

    Time: 5 ms (6.93%) | Memory: 42.7 MB (9.92%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,80 @@
+class Solution {
+    Set<List<String>> ans = new HashSet<>();
+
+    private void build(boolean[][] row) {
+
+        List<String> ls = new ArrayList<>();
+        for (int i = 0; i < row.length; i++) {
+            StringBuilder s = new StringBuilder();
+            for (int j = 0; j < row.length; j++) {
+                if (row[i][j]) {
+                    s.append('Q');
+                } else {
+                    s.append('.');
+                }
+            }
+            ls.add(s.toString());
+        }
+        ans.add(new ArrayList(ls));
+    }
+
+    private boolean check(boolean[][] board, int i, int j) {
+
+        for (int r = 0; r < i; r++) {
+            if (board[r][j])
+                return false;
+        }
+
+        for (int r = i - 1, c = j - 1; r >= 0 && c >= 0; r--, c--) {
+            if (board[r][c])
+                return false;
+        }
+
+        for (int r = i - 1, c = j + 1; r >= 0 && c < board.length; r--, c++) {
+            if (board[r][c])
+                return false;
+        }
+
+        return true;
+    }
+
+    private void helper(int n, int i, int j, boolean[][] row) {
+
+        if (n == 0) {
+            build(row);
+            return;
+        }
+        if (i >= row.length || j >= row.length) {
+            return;
+        }
+        if (check(row, i, j)) {
+            row[i][j] = true;
+            if (i == row.length - 1) {
+                helper(n - 1, i + 1, 0, row);
+            } else {
+                for (int a = 0; a < row.length; a++) {
+                    if (check(row, i + 1, a)) {
+                        helper(n - 1, i + 1, a, row);
+                    }
+                }
+            }
+            row[i][j] = false;
+
+        }
+        return;
+
+    }
+
+    public int totalNQueens(int n) {
+         ans.clear();
+        for (int i = 0; i < n; i++) {
+            boolean[][] row = new boolean[n][n];
+
+            helper(n, 0, i, row);
+
+        }
+       
+
+        return ans.size();
+    }
+}
