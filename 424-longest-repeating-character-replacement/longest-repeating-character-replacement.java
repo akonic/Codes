cb2d57c770925fcb28741a95fc4f4e7f7ce5f685
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-04 23:00:29 +0530

    Time: 32 ms (16.28%) | Memory: 46.8 MB (26.52%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,54 @@
+class Solution {
+    private int check(boolean[][] mp, int a, int k) {
+        int temp = k;
+        int n = mp[a].length;
+        int i = 0, j = 0;
+        int c = 0, x = 0;
+        while (i < n && j < n) {
+            
+            if(mp[a][j])
+            {
+                c++;
+                j++;
+            }
+            else if(!mp[a][j])
+            {
+                if(temp>0)
+                {
+                    temp--;c++;j++;
+                }
+                else{
+                    if(!mp[a][i])
+                    {
+                        i++;temp++;c--;
+                    }
+                    else{
+                        while(i<n && mp[a][i])
+                        {
+                            i++;c--;
+                        }
+                    }
+                }
+            }
+        x = Math.max(x, c);
+            
+        } //x = Math.max(x, c);
+        
+        return x;
+    }
+
+    public int characterReplacement(String s, int k) {
+        char[] ch = s.toCharArray();
+        int n = ch.length;
+        boolean[][] mp = new boolean[26][n];
+        for (int i = 0; i < n; i++) {
+            mp[ch[i] - 'A'][i] = true;
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            ans = Math.max(ans, check(mp, i, k));
+           // System.out.println("----------");
+        }
+        return ans;
+    }
+}
