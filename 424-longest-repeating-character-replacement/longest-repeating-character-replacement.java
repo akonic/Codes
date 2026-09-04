adfa5df08bf9fa4d11ff202f8da89456eaa43988
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-04 23:02:46 +0530

    Time: 12 ms (44.70%) | Memory: 46.4 MB (30.92%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,49 +5,45 @@ class Solution {
         int i = 0, j = 0;
         int c = 0, x = 0;
         while (i < n && j < n) {
-            
-            if(mp[a][j])
-            {
+
+            if (mp[a][j]) {
                 c++;
                 j++;
-            }
-            else if(!mp[a][j])
-            {
-                if(temp>0)
-                {
-                    temp--;c++;j++;
-                }
-                else{
-                    if(!mp[a][i])
-                    {
-                        i++;temp++;c--;
+            } else if (!mp[a][j]) {
+                if (temp > 0) {
+                    temp--;
+                    c++;
+                    j++;
+                } else {
+                    if (!mp[a][i]) {
+                        temp++;
                     }
-                    else{
-                        while(i<n && mp[a][i])
-                        {
-                            i++;c--;
-                        }
-                    }
+                    i++;
+                    c--;
                 }
             }
-        x = Math.max(x, c);
-            
-        } //x = Math.max(x, c);
-        
+            x = Math.max(x, c);
+
+        }
+
         return x;
     }
 
     public int characterReplacement(String s, int k) {
         char[] ch = s.toCharArray();
         int n = ch.length;
+        boolean[] isPresent = new boolean[26];
         boolean[][] mp = new boolean[26][n];
         for (int i = 0; i < n; i++) {
             mp[ch[i] - 'A'][i] = true;
+            isPresent[ch[i] - 'A'] = true;
         }
         int ans = 0;
         for (int i = 0; i < 26; i++) {
-            ans = Math.max(ans, check(mp, i, k));
-           // System.out.println("----------");
+            if (isPresent[i]) {
+                ans = Math.max(ans, check(mp, i, k));
+            }
+            // System.out.println("----------");
         }
         return ans;
     }
