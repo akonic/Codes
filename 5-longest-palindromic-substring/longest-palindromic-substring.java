76bc29a354a272419d0201314f4d755b167e4b4a
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-05 15:18:26 +0530

    Time: 583 ms (13.65%) | Memory: 46.7 MB (34.97%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,44 +1,37 @@
 class Solution {
-    private boolean check(char[] ch, int j, int len) {
-        int i = j - len + 1;
-        while (i < j) {
-            if (ch[i] == ch[j]) {
-                i++;
-                j--;
-            } else {
+    private boolean check(char[] ch,int i,int j)
+    {
+        while(i<j)
+        {
+            if(ch[i]!=ch[j])
+            {
                 return false;
             }
+            i++;j--;
         }
         return true;
     }
-
     public String longestPalindrome(String s) {
         char[] ch = s.toCharArray();
         int n = ch.length;
-        char[] bh = new char[n];
-        for (int i = 0; i < n; i++) {
-            bh[i] = ch[n - i - 1];
-        }
-        int[][] dp = new int[n + 1][n + 1];
-        int ans = 0, x = -1, y = -1;
-        int maxLen = 1;
-        int end = 0;
-        List<int[]> ls = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (ch[i - 1] == bh[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                int len = dp[i][j];
-                int originalEnd = i - 1;
-
-                if (len > maxLen && check(ch, originalEnd, len)) {
-                    maxLen = len;
-                    end = originalEnd;
+        int ans=0;
+        String st ="";
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(check(ch,i,j) && j-i+1>ans)
+                {
+                    ans=j-i+1;
+                    StringBuilder p = new StringBuilder();
+                    for(int u=i;u<=j;u++)
+                    {
+                        p.append(ch[u]);
+                    }
+                    st = p.toString();
                 }
             }
         }
-
-        return s.substring(end - maxLen + 1, end + 1);
+        return st;
     }
 }
