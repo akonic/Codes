382a842e81a9e0694a5972f1d1b3bbf4ce284a81
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-13 15:32:15 +0530

    Time: 68 ms (33.71%) | Memory: 47 MB (48.48%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -4,21 +4,29 @@ class Solution {
         List<int[]> ls = new ArrayList<>();
         List<int[]> bs = new ArrayList<>();
 
-        Map<String,Integer> mp = new HashMap<>();
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(img1[i][j]==1){ls.add(new int[]{i,j});};
-                if(img2[i][j]==1){bs.add(new int[]{i,j});};
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (img1[i][j] == 1) {
+                    ls.add(new int[] { i, j });
+                }
+                ;
+                if (img2[i][j] == 1) {
+                    bs.add(new int[] { i, j });
+                }
+                ;
             }
         }
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int[] a : ls) {
             for (int[] b : bs) {
-                String key = (b[0] - a[0]) + "," + (b[1] - a[1]);
-                int cnt = mp.getOrDefault(key, 0) + 1;
-                mp.put(key, cnt);
+                int dx = b[0] - a[0] + 30;
+                int dy = b[1] - a[1] + 30;
+                int key = dx * 100 + dy;
+
+                int cnt = map.getOrDefault(key, 0) + 1;
+                map.put(key, cnt);
                 ans = Math.max(ans, cnt);
             }
         }
