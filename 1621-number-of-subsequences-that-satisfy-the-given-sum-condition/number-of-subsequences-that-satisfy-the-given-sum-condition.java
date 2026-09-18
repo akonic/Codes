477385e42fd3b63b79eda7985da71f128d3db97a
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-19 01:31:15 +0530

    Time: 33 ms (59.18%) | Memory: 66.8 MB (22.27%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,34 +1,21 @@
 class Solution {
     public static final int MOD = 1_000_000_007;
 
-    public static int powerModulo(int base, int exp) {
-        int result = 1;
-
-        base = (base % MOD + MOD) % MOD;
-
-        while (exp > 0) {
-            if ((exp & 1) == 1) {
-                result = (int) (((long) result * base) % MOD);
-            }
-            base = (int) (((long) base * base) % MOD);
-            exp = exp >> 1;
-        }
-        return result;
-    }
-
     public int numSubseq(int[] nums, int target) {
         int ans = 0;
         Arrays.sort(nums);
         int n = nums.length;
         int i = 0;
         int j = n - 1;
+        int[] powerOf2 = new int[n];
+        powerOf2[0] = 1;
+        for (int k = 1; k < n; k++) {
+            powerOf2[k] = (powerOf2[k - 1] * 2) % MOD;
+        }
         while (i <= j) {
             if (nums[i] + nums[j] <= target) {
                 int len = j - i;
-
-                int contribution = powerModulo(2, len);
-
-                ans = (ans + contribution) % MOD;
+                ans = (ans + powerOf2[len]) % MOD;
 
                 i++;
             } else {
