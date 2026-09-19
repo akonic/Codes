705b1461933fe1dd72cb0b6e2c7dec02fc7b5b6c
Ankit Kachhi <45717770+akonic@users.noreply.github.com>
2026-09-19 23:13:09 +0530

    Time: 46 ms (10.27%) | Memory: 138.4 MB (5.48%) - LeetSync

--- 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.java
+++ 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.java
@@ -2,20 +2,18 @@ class Solution {
      public static int powerMod(int base, int exponent) {
         int MOD = 1000000007; 
         long result = 1;     
-        long b = base % MOD;  // Kept as long for multiplication safety
+        long b = base % MOD; 
 
         while (exponent > 0) {
-            // If exponent is odd, multiply b with the result
             if (exponent % 2 == 1) {
                 result = (result * b) % MOD;
             }
             
-            // Square the base and halve the exponent
             b = (b * b) % MOD;
             exponent = exponent / 2;
         }
         
-        return (int) result; // Safely cast back to int
+        return (int) result; 
     }
     public int numberOfGoodPartitions(int[] nums) {
         int n = nums.length;
@@ -24,13 +22,11 @@ class Solution {
         {
             mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
         }
-        //List<Integer> ls = new ArrayList();
         int count=0;
         int i=0,j=0;
         while(i<n && j<n)
         {
             int k = mp.get(nums[i]).get(0);
-            //System.out.println(i+" "+j+" "+k);
             int mx=k;
             while(j<mx && j<n)
             {
